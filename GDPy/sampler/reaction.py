fee80bb1c38ad5125c8097f7daa48b428ddab5c8
hsulab <ahcigar@foxmail.com>
2022-01-18 12:15:15 +0000

    add a placeholder for reaction sampling

---
+++ GDPy/sampler/reaction.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from GDPy.sampler.abstract import AbstractExplorer
+
+
+class ReactionExplorer(AbstractExplorer):
+
+    def icreate(self):
+        """ create and submit exploration tasks
+        """
+
+        return
+
+
+if __name__ == "__main__":
+    pass
