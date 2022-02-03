b3c1a1d8a67763b507fab8a22477a13ec2fa1f0b
hsulab <ahcigar@foxmail.com>
2022-02-03 15:34:55 +0000

    add abstract dynamics

---
+++ GDPy/calculator/dynamics.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import abc
+import copy
+import pathlib
+
+from typing import Optional
+from collections.abc import Iterable
+
+
+class AbstractDynamics(abc.ABC):
+
+    delete = []
+    keyword: Optional[str] = None
+    special_keywords = {}
+
+    def __init__(self, calc, directory, *args, **kwargs):
+
+        self.calc = calc
+        self.calc.reset()
+
+        self._directory_path = pathlib.Path(directory)
+
+        return
+
+    def set_output_path(self, directory):
+        """"""
+        self._directory_path = pathlib.Path(directory)
+
+        return
+    
+    def reset(self):
+        """ remove results stored in dynamics calculator
+        """
+        self.calc.reset()
+
+        return
+
+    def delete_keywords(self, kwargs):
+        """removes list of keywords (delete) from kwargs"""
+        for d in self.delete:
+            kwargs.pop(d, None)
+
+        return
+
+    def set_keywords(self, kwargs):
+        # TODO: rewrite this method
+        args = kwargs.pop(self.keyword, [])
+        if isinstance(args, str):
+            args = [args]
+        elif isinstance(args, Iterable):
+            args = list(args)
+
+        for key, template in self.special_keywords.items():
+            if key in kwargs:
+                val = kwargs.pop(key)
+                args.append(template.format(val))
+
+        kwargs[self.keyword] = args
+
+        return
+
+    @abc.abstractmethod
+    def run(self, atoms, **kwargs):
+        """"""
+
+
+        return 
+
+
+if __name__ == "__main__":
+    pass
