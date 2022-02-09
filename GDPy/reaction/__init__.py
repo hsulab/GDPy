3d52d1b34edbc1e2b71da216f68b4e220afde7f5
hsulab <ahcigar@foxmail.com>
2022-02-09 16:58:13 +0000

    change this submodule to reaction

---
+++ GDPy/reaction/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" This submodule is for exploring, sampling, 
+    and performing (chemical) reactions with
+    various advanced algorithms.
+"""
+
