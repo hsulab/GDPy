72e07020808bb0fb0862ad7ef9cdab2b59d890d1
hsulab <ahcigar@foxmail.com>
2022-02-09 19:11:36 +0000

    another version of selection

---
+++ GDPy/selector/cur.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from scipy.sparse.linalg import LinearOperator, svds
+
+
+def descriptor_svd(at_descs, num, do_vectors='vh'):
+    def mv(v):
+        return np.dot(at_descs, v)
+    def rmv(v):
+        return np.dot(at_descs.T, v)
+
+    A = LinearOperator(at_descs.shape, matvec=mv, rmatvec=rmv, matmat=mv)
+
+    return svds(A, k=num, return_singular_vectors=do_vectors) # sparse SVD
+
+def cur_selection(features, num, zeta=2, strategy="descent"):
+    """ input should be [nframes,nfeatures]
+    """
+    # column vectors of descriptors
+    assert features.ndim == 2
+    nframes = features.shape[0]
+
+    at_descs = features.copy().T
+
+    # do SVD on kernel if desired
+    if zeta > 0.0:
+        m = np.matmul(at_descs.T, at_descs)**zeta
+    else:
+        m = at_descs
+
+    #print("Number of frames ", nframes)
+    #print("The descriptor vector shape ", at_descs.shape)
+    #print("The similarity matrix shape ", m.shape)
+    #print("Number of selected ", num)
+
+    (u, s, vt) = descriptor_svd(m, min(max(1,int(num/2)),min(m.shape)-1))
+    c_scores = np.sum(vt**2, axis=0) / vt.shape[0]
+
+    if strategy == 'stochastic':
+        selected = sorted(
+            np.random.choice(range(nframes), size=num, replace=False, p=c_scores)
+        )
+    elif strategy == 'descent':
+        selected = sorted(np.argsort(c_scores)[-num:])
+    else:
+        raise ValueError('Unsupport CUR selection strategy.')
+
+    return c_scores, selected
