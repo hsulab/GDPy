b701e33c9fa9dc4c073cc89bfd17bfa32ae79e35
hsulab <ahcigar@foxmail.com>
2022-08-04 19:24:13 +0100

    move some early expedition functions to builder

--- GDPy/builder/utils.py
+++ GDPy/builder/utils.py
@@ -54,6 +54,23 @@ def per_main():
 
     pass
 
+def sort_atoms(atoms):
+    # sort atoms by symbols and z-positions especially for supercells 
+    numbers = atoms.numbers 
+    zposes = atoms.positions[:,2].tolist()
+    sorted_indices = np.lexsort((zposes,numbers))
+    atoms = atoms[sorted_indices]
+
+    return atoms
+
+init_stru = './opts/Pt3O4_opt.xyz'
+atoms = read(init_stru)
+atoms = make_supercell(atoms, 2.0*np.eye(3)) # (2x2x2) cell
+atoms = sort_atoms(atoms)
+print(atoms.cell)
+
+write('Pt32.data', atoms, format='lammps-data')
+
 
 if __name__ == '__main__':
     pass
