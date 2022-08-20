6321674e0d7c99a2e239e00670288184a53415b0
hsulab <ahcigar@foxmail.com>
2022-08-20 15:53:59 +0100

    update local machine

--- GDPy/machine/factory.py
+++ GDPy/machine/factory.py
@@ -8,14 +8,18 @@
 def create_machine(params):
     """
     """
-    backend = params.pop("backend", None)
-    if backend == "slurm":
-        from GDPy.machine.machine import SlurmMachine
-        machine = SlurmMachine(**params)
-    elif backend == "pbs":
-        from GDPy.machine.machine import PbsMachine
-        pass
+    if params is None:
+        from GDPy.machine.machine import LocalMachine
+        machine = LocalMachine()
     else:
-        pass
+        backend = params.pop("backend", None)
+        if backend == "slurm":
+            from GDPy.machine.machine import SlurmMachine
+            machine = SlurmMachine(**params)
+        elif backend == "pbs":
+            from GDPy.machine.machine import PbsMachine
+            pass
+        else:
+            pass
 
     return machine
