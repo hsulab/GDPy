aedd2eb381e44b3cc6f72fcfccdc5da37415a1a6
hsulab <ahcigar@foxmail.com>
2022-09-01 20:07:20 +0100

    rename

--- GDPy/expedition/__init__.py
+++ GDPy/expedition/__init__.py
@@ -1,6 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from .md import MDBasedExpedition
-#from .randomSearch import RandomExplorer
-from .interface import run_expedition
+from GDPy.utils.command import parse_input_file
+
+
+def run_expedition(potter, referee, exp_json, chosen_step, global_params = None):
+    # - create exploration
+    exp_dict = parse_input_file(exp_json)
+
+    method = exp_dict.get("method", "MD")
+    if method == "MD":
+        from .md import MDBasedExpedition as exp_cls
+    elif method == "GA":
+        from .randomSearch import RandomExplorer as exp_cls
+    elif method == "adsorbate":
+        from .adsorbate import AdsorbateEvolution as exp_cls
+    elif method == "reaction":
+        from .reaction import ReactionExplorer as exp_cls
+    elif method == "otf":
+        from .online.md import OnlineDynamicsBasedExpedition as exp_cls
+    else:
+        raise NotImplementedError(f"Method {method} is not supported.")
+
+    scout = exp_cls(exp_dict, potter, referee)
+
+    # - adjust global params
+    print("optional params ", global_params)
+    if global_params is not None:
+        assert len(global_params)%2 == 0, "optional params must be key-pair"
+        for first in range(0, len(global_params), 2):
+            print(global_params[first], " -> ", global_params[first+1])
+            scout.default_params[chosen_step][global_params[first]] = eval(global_params[first+1])
+
+    # - run
+    op_name = "i" + chosen_step
+    assert isinstance(op_name, str), "op_nam must be a string"
+    op = getattr(scout, op_name, None)
+    if op is not None:
+        scout.run(op, "./")
+    else:
+        raise ValueError("Wrong chosen step %s..." %op_name)
+
+    return
+
+
+if __name__ == "__main__":
+    pass
