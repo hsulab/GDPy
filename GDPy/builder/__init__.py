a8d2d5aa0dc04570913bd1af0bf73b45af968844
hsulab <ahcigar@foxmail.com>
2022-09-23 13:08:29 +0100

    support direct and formula generators

--- GDPy/builder/__init__.py
+++ GDPy/builder/__init__.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
+from GDPy.builder.direct import DirectGenerator
+from GDPy.builder.species import FormulaBasedGenerator
 from GDPy.builder.builder import StructureGenerator
 from GDPy.builder.randomBuilder import RandomGenerator
 from GDPy.builder.adsorbate import AdsorbateGraphGenerator
 
+supported_filetypes = [".xyz", ".arc"]
 
 def create_generator(params) -> StructureGenerator:
     """"""
+    # - parse string
+    if isinstance(params, str):
+        suffix = params[-4:]
+        if suffix in supported_filetypes:
+            from ase.io import read, write
+            params = dict(
+                method = "direct",
+                frames = read(params, ":")
+            )
+        else:
+            params = dict(
+                method = "formula",
+                chemical_formula = params
+            )
+
+    # - params dict
     method = params.pop("method", "random")
-    if method == "random":
+    if method == "direct":
+        generator = DirectGenerator(**params)
+    elif method == "formula":
+        generator = FormulaBasedGenerator(**params)
+    elif method == "random":
         generator = RandomGenerator(params)
     elif method == "adsorbate":
         generator = AdsorbateGraphGenerator(**params)
+    else:
+        raise RuntimeError("Unknown generator params...")
 
     return generator
     
