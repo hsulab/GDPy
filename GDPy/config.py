f9667a3381f4beaf0e43a3d7a74576633b49a742
hsulab <ahcigar@foxmail.com>
2022-10-09 21:08:28 +0100

    add a few shared parameters

--- GDPy/config.py
+++ GDPy/config.py
@@ -1,5 +1,23 @@
-""" some shared parameters
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+"""Some shared configuration parameters.
 """
+
 from typing import Union
 
+#: number of parallel jobs for joblib
 NJOBS: int = 1
+
+# - find default vasp settings
+#gdpconfig = Path.home() / ".gdp"
+#if gdpconfig.exists() and gdpconfig.is_dir():
+#    # find vasp config
+#    vasprc = gdpconfig / "vasprc.json"
+#    with open(vasprc, "r") as fopen:
+#        input_dict = json.load(fopen)
+#else:
+#    input_dict = {}
+
+if __name__ == "__main__":
+    pass
