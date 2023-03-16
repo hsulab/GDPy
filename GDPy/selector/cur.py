ad8deb403f8ed8bc75e115d2759b66b76b7eff2c
hsulab <ahcigar@foxmail.com>
2023-03-16 23:04:45 +0000

    add farthest point sampling

--- GDPy/selector/cur.py
+++ GDPy/selector/cur.py
@@ -3,8 +3,12 @@
 
 import numpy as np
 from scipy.sparse.linalg import LinearOperator, svds
+from scipy.spatial.distance import cdist
 
+"""Methods for selection of vector-based descriptors.
+"""
 
+# - CUR decomposition
 def descriptor_svd(at_descs, num: int, do_vectors="vh"):
     """Perfrom a sparse SVD."""
     def mv(v):
@@ -52,6 +56,40 @@ def cur_selection(features, num: int, zeta: float=2, strategy: str="descent", rn
         raise ValueError('Unsupport CUR selection strategy.')
 
     return c_scores, selected
+
+# - FPS (Farthest Point Sampling)
+def fps_selection(features, num: int, min_distance=0.1, metric="euclidean", metric_params={}, rng=np.random):
+    """Farthest point sampling on vector-based features.
+
+    Reference:
+        https://github.com/bigd4/PyNEP.git
+
+    """
+    npoints = features.shape[0]
+
+    selected_indices, selected_features = [], []
+
+    # - get starting point
+    start_index = rng.choice(npoints)
+    selected_indices.append(start_index)
+    selected_features.append(features[start_index])
+
+    distances = np.min(
+        cdist(features, selected_features, metric=metric, **metric_params), axis=1
+    ) # shape (npoints,nselected) -> (npoints,)
+
+    while np.max(distances) > min_distance or len(selected_indices) < num:
+        i = np.argmax(distances)
+        selected_indices.append(i)
+        if len(selected_indices) >= num:
+            break
+        distances = np.minimum(
+            distances, cdist([features[i]], features, metric=metric, **metric_params)[0]
+        ) # shape (npoints,)
+    
+    scores = distances
+
+    return scores, selected_indices
 
 if __name__ == "__main__":
     pass
