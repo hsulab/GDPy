5d3e935ae34007e9407960ee46af83074d241d43
hsulab <ahcigar@foxmail.com>
2023-03-20 22:28:53 +0000

    update node

--- GDPy/core/node.py
+++ GDPy/core/node.py
@@ -2,13 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import copy
 import pathlib
 from typing import NoReturn, Union
+
+import numpy as np
 
 """Every working component is represented by a node.
 """
 
 class AbstractNode(abc.ABC):
+
+    #: Node name.
+    name: str = "node"
 
     #: Working directory.
     _directory: pathlib.Path = "./"
@@ -16,9 +22,22 @@ class AbstractNode(abc.ABC):
     #: Standard print function.
     _print = print
 
-    def __init__(self, directory: Union[str,pathlib.Path]="./", *args, **kwargs):
+    #: Default parameters.
+    default_parameters: dict = dict()
+
+    def __init__(self, directory: Union[str,pathlib.Path]="./", random_seed=1112, *args, **kwargs):
         """"""
+        # - set working directory
         self.directory = directory
+
+        # - set random generator
+        self.set_rng(seed=random_seed)
+
+        # - update parameters from kwargs
+        self.parameters = copy.deepcopy(self.default_parameters)
+        for k in self.parameters:
+            if k in kwargs.keys():
+                self.parameters[k] = kwargs[k]
 
         return
 
@@ -39,6 +58,126 @@ class AbstractNode(abc.ABC):
         #    self._directory.mkdir(parents=True)
 
         return 
+
+    def set_rng(self, seed=None):
+        """"""
+        # - save random seed
+        self.random_seed = seed
+
+        # - assign random seeds
+        if seed is None:
+            self.rng = np.random.default_rng()
+        elif isinstance(seed, int):
+            self.rng = np.random.default_rng(seed)
+
+        return
+
+    def set(self, *args, **kwargs):
+        """Set parameters."""
+        for k, v in kwargs.items():
+            if k in self.parameters:
+                self.parameters[k] = v
+
+        return
+
+    def __getattr__(self, key):
+        """Corresponding getattribute-function."""
+        if key != "parameters" and key in self.parameters:
+            return self.parameters[key]
+
+        return object.__getattribute__(self, key)
+
+    def as_dict(self) -> dict:
+        """Return a dict that stores the current state of this node."""
+        params = dict(
+            name = self.__class__.__name__
+        )
+        params.update(**copy.deepcopy(self.parameters))
+
+        return params
+
+
+class AbstractGraph(abc.ABC):
+
+    #: Node name.
+    name: str = "graph"
+
+    #: Working directory.
+    _directory: pathlib.Path = "./"
+
+    #: Standard print function.
+    _print = print
+
+    #: Default parameters.
+    default_parameters: dict = dict()
+
+    def __init__(self, directory: Union[str,pathlib.Path]="./", random_seed=1112, *args, **kwargs):
+        """"""
+        # - set working directory
+        self.directory = directory
+
+        # - set random generator
+        self.set_rng(seed=random_seed)
+
+        # - update parameters from kwargs
+        self.parameters = copy.deepcopy(self.default_parameters)
+        for k in self.parameters:
+            if k in kwargs.keys():
+                self.parameters[k] = kwargs[k]
+
+        return
+
+    @property
+    def directory(self) -> pathlib.Path:
+        """Working directory.
+
+        Note:
+            When setting directory, some additional files are re-directed.
+
+        """
+        return self._directory
+    
+    @directory.setter
+    def directory(self, directory_: Union[str,pathlib.Path]) -> NoReturn:
+        self._directory = pathlib.Path(directory_)
+        #if not self._directory.exists():
+        #    self._directory.mkdir(parents=True)
+
+        return 
+
+    def set_rng(self, seed=None):
+        """"""
+        # - assign random seeds
+        if seed is None:
+            self.rng = np.random.default_rng()
+        elif isinstance(seed, int):
+            self.rng = np.random.default_rng(seed)
+
+        return
+
+    def set(self, *args, **kwargs):
+        """Set parameters."""
+        for k, v in kwargs.items():
+            if k in self.parameters:
+                self.parameters[k] = v
+
+        return
+
+    def __getattr__(self, key):
+        """Corresponding getattribute-function."""
+        if key != "parameters" and key in self.parameters:
+            return self.parameters[key]
+
+        return object.__getattribute__(self, key)
+
+    def as_dict(self) -> dict:
+        """Return a dict that stores the current state of this node."""
+        params = dict(
+            name = self.__class__.__name__
+        )
+        params.update(**copy.deepcopy(self.parameters))
+
+        return params
 
 
 def create_node(params: dict):
