adb5ae1784e2efe6b3b18c72b19048995030904b
hsulab <ahcigar@foxmail.com>
2023-04-30 01:22:43 +0100

    add molecule builder

--- GDPy/builder/species.py
+++ GDPy/builder/species.py
@@ -8,12 +8,14 @@ import numpy as np
 
 import ase
 from ase import Atoms
+from ase.io import read, write
 from ase.collections import g2
 from ase.build import molecule
 
 from ase.neighborlist import natural_cutoffs, NeighborList
 
-from GDPy.builder.builder import StructureGenerator
+from GDPy.core.register import registers
+from GDPy.builder.builder import StructureGenerator, StructureBuilder
 
 class FormulaBasedGenerator(StructureGenerator):
 
@@ -60,6 +62,78 @@ def build_species(species):
         raise ValueError(f"Cant create species {species}")
 
     return atoms
+
+@registers.builder.register
+class MoleculeBuilder(StructureBuilder):
+
+    name: str = "molecule"
+
+    #: Default box for molecule to stay in. 
+    default_box = np.eye(3)*20.
+
+    #: Whether centre the molecule.
+    recentre: bool = True
+
+    def __init__(self, name: str=None, filename: str=None, directory="./", *args, **kwargs):
+        """"""
+        super().__init__(directory=directory, *args, **kwargs)
+
+        if (name is None and filename is None) or (name is not None and filename is not None):
+            raise RuntimeError(f"Either name or filname should be set for {self.name}.")
+        self.name, self.func = name, self._build
+        self.filename, self.func = filename, self._read
+
+        return
+    
+    def run(self, *args, **kwargs) -> List[Atoms]:
+        """"""
+        super().run(*args, **kwargs)
+
+        atoms = self.func()
+
+        # - check box
+        #   NOTE: for molecule without cell,
+        #         atoms.get_cell() returns [0.,0.,0.] while atoms.get_cell(complete=True)
+        #         returns [1.,1.,1.]
+        cell = atoms.get_cell()
+        if np.sum(np.fabs(cell)) < 1e-8:
+            # The box is too small.
+            atoms.set_cell(self.default_box)
+        else:
+            ...
+        
+        # - centre the molecule
+        cell = atoms.get_cell(complete=True)
+        if self.recentre:
+            box_centre = np.sum(cell, axis=0)/2.
+            cop = np.average(atoms.get_positions(), axis=0)
+            atoms.translate(box_centre-cop)
+            ...
+
+        return [atoms]
+    
+    def _read(self) -> Atoms:
+        """"""
+        frames = read(self.filename, ":")
+        nframes = len(frames)
+        assert nframes == 1, "Only one structure is accepted."
+
+        return frames[0]
+    
+    def _build(self) -> Atoms:
+        """"""
+        molecule_name = self.name
+        # - build adsorbate
+        atoms = None
+        if molecule_name in ase.data.chemical_symbols:
+            atoms = Atoms(molecule_name, positions=[[0.,0.,0.]])
+        elif species in g2.names:
+            atoms = molecule(molecule_name)
+        else:
+            raise RuntimeError(f"Cant create molecule {molecule_name}")
+
+        return atoms
+
 
 # ===== ====== ===== ===== ====== ===== ===== ====== ===== =====
 # Water Molecules Number Calculation @ 298.15K
