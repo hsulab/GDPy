c258cbef7f4b3470fd09f5d2c9dce28cb27deb27
hsulab <ahcigar@foxmail.com>
2023-05-19 20:59:40 +0100

    implement hypercube on internal coordinates

--- GDPy/builder/hypercube.py
+++ GDPy/builder/hypercube.py
@@ -1,58 +1,207 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import copy
 from typing import List
+import warnings
 
 import numpy as np
+from scipy.stats import qmc
+
+import jax
+import jax.numpy as jnp
+from jax import grad, value_and_grad, jit, jacfwd, jacrev
 
 from ase import Atoms
 from ase.io import read, write
 
-from GDPy.builder.builder import StructureGenerator
+from GDPy.core.register import registers
+from GDPy.builder.builder import StructureBuilder
 
 """Sample small molecule.
 """
 
-class HypercubeGenerator(StructureGenerator):
+@jit
+def bond_distance(positions, indices: List[int]):
+    """"""
+    a1, a2 = indices
+    dis = jnp.linalg.norm(positions[a2]-positions[a1])
 
-    def __init__(self, directory="./", *args, **kwargs):
-        super().__init__(directory, *args, **kwargs)
+    return dis
+grad_bond_distance = grad(bond_distance, argnums=0)
+
+@jit
+def compute_bond_distances(positions: np.array, pair_indices: np.array):
+    """Compute distances.
+
+    Args:
+        positions: Positions with shape of (natoms,3).
+        pair_distances: Indices of atoms for distances [[0,1],[0,2]].
     
-    def run(self, *args, **kwargs) -> List[Atoms]:
+    Examples:
+
+        >>> pairs = np.array([[0,1]])
+        >>> distances = compute_bond_distances(positions, pairs)
+    
+    Returns:
+        An array of distances.
+
+    """
+    pair_positions = jnp.take(positions, pair_indices.T, axis=0)
+    dvecs = pair_positions[0] - pair_positions[1] # TODO: + pair_shifts
+    distances = jnp.linalg.norm(dvecs, axis=1)
+
+    return distances
+
+@jit
+def bond_angle(positions, indices):
+    """"""
+    a1, a2, a3 = indices
+    dvec1 = positions[a2] - positions[a1]
+    dvec2 = positions[a3] - positions[a1]
+
+    #angle = jnp.arccos(jnp.dot(dvec1,dvec2)/jnp.linalg.norm(dvec1)/jnp.linalg.norm(dvec2))
+    angle = jnp.arctan2(jnp.linalg.norm(jnp.cross(dvec1,dvec2)), jnp.dot(dvec1,dvec2))
+
+    return angle
+
+@jit
+def compute_bond_angles(positions, trimer_indices):
+    """Compute angles.
+
+    For very small/acute angles, results by arccos are inaccurate. arctan may be 
+    more effective.
+
+    """
+    trimer_positions = jnp.take(positions, trimer_indices.T, axis=0)
+    # TODO: shifts
+    dvecs1 = trimer_positions[1] - trimer_positions[0]
+    dnorms1 = jnp.linalg.norm(dvecs1, axis=1)
+    dvecs2 = trimer_positions[2] - trimer_positions[0]
+    dnorms2 = jnp.linalg.norm(dvecs2, axis=1)
+
+    angles = jnp.arccos(jnp.sum(dvecs1*dvecs2, axis=1) / dnorms1 / dnorms2)
+
+    return angles
+
+@jit
+def pseudo_inverse_of_jacobian(jac, eps=0.0001):
+    """"""
+    dim = jac.shape[0]
+    jac_inv = jnp.transpose(jac)@jnp.linalg.inv(jac@jnp.transpose(jac)+eps*jnp.eye(dim))
+
+    return jac_inv
+
+@registers.builder.register
+class HypercubeBuilder(StructureBuilder):
+
+    """Use hypercube sampling to generate internal coordinates.
+
+    The implementation is not exact the same as the references. Here, we only 
+    use hypercube to generate structures. No symmetry constraints is applied.
+
+    TODO: 
+        * Support dihedrals.
+
+    References:
+        [1] J. Chem. Phys. 2017, 147, 161706.
+        [2] J. Chem. Phys. 2018, 149, 174114.
+
+    """
+
+    #: Maximum number of updates of positions.
+    MAX_ATTEMPTS_UPDATE: int = 100
+
+    #: Tolerance of target internal coordinates.
+    TOL_INTCOORD: float = 1e-4
+
+    def __init__(
+        self, distances, disrange: List[float], 
+        angles=None, angrange: List[float]=None, 
+        directory="./", random_seed=1112, *args, **kwargs
+    ):
         """"""
+        super().__init__(directory, random_seed, *args, **kwargs)
+
+        self.dimers = np.array(distances)
+        self.trimers = np.array(angles)
+
+        self.ndof = len(self.dimers) + len(self.trimers)
+
+        bounds_ = []
+        if disrange:
+            if isinstance(disrange[0], list):
+                bounds_.extend(disrange)
+            else: # two floats
+                bounds_.extend([disrange]*len(self.dimers))
+        if self.trimers is not None:
+            if angrange:
+                if isinstance(angrange[0], list):
+                    bounds_.extend(angrange)
+                else:
+                    bounds_.extend([angrange]*len(self.trimers))
+            else:
+                ... # TODO: set default 15,165?
+        self.bounds = np.array(bounds_).T
+
+        assert self.bounds.shape[1] == self.ndof, "Inconsistent number of freedoms and ranges."
 
         return
+    
+    def run(self, substrate: Atoms, size: int, *args, **kwargs) -> List[Atoms]:
+        """Modify input structures to generate random hypercube structures.
+        """
+        # NOTE: strength new in 1.8.0
+        #sampler = qmc.LatinHypercube(d=ninternals, strength=2, seed=self.rng) 
+        sampler = qmc.LatinHypercube(d=self.ndof, seed=self.rng)
+        samples = sampler.random(n=size)
 
+        l_bounds, u_bounds = self.bounds
+        scaled_samples = qmc.scale(samples, l_bounds, u_bounds)
+
+        # - generate structures
+        frames = []
+        for sample in scaled_samples:
+            curr_atoms = copy.deepcopy(substrate)
+            curr_atoms = self._approx_structure(curr_atoms, sample, self.dimers, self.trimers)
+            frames.append(curr_atoms)
+
+        return frames
+    
+    def _approx_structure(self, atoms: Atoms, targets, pairs, trimers=None):
+        """"""
+        natoms = len(atoms)
+
+        # - compute pseudo inverse of the jacobian matrix
+        positions = copy.deepcopy(atoms.positions)
+        for i in range(self.MAX_ATTEMPTS_UPDATE):
+            distances = compute_bond_distances(positions, pairs)
+            if trimers is not None:
+                angles = compute_bond_angles(positions, trimers)
+            else:
+                angles = []
+            internals = np.hstack((distances,angles))
+            disp = targets - internals 
+            if np.max(np.fabs(disp)) < self.TOL_INTCOORD:
+                break
+            dis_jac_ = jacrev(compute_bond_distances, argnums=0)(positions, pairs)
+            dis_jac = dis_jac_.reshape(-1,natoms*3)
+            if trimers is not None:
+                ang_jac_ = jacrev(compute_bond_angles, argnums=0)(positions, trimers)
+                ang_jac = ang_jac_.reshape(-1,natoms*3)
+            else:
+                ang_jac = []
+            jac = np.vstack((dis_jac,ang_jac))
+            jac_inv = pseudo_inverse_of_jacobian(jac)
+            positions = copy.deepcopy(positions) + jnp.reshape(jac_inv@disp, (-1,3))
+        else:
+            warnings.warn("Iterative approximation is not converged.", UserWarning)
+        
+        # - update positions
+        atoms.positions = positions
+        
+        return atoms
 
 
 if __name__ == "__main__":
-    # We'd like to sample CO2 (a trimer) with different distances and angles
-    atoms_ = Atoms(
-        "CO2", positions=[[0.,0.,0.],[1.2,0.,0.],[-1.2,0.,0.]], cell=10.*np.eye(3),
-        pbc=True
-    )
-
-    npoints = 8
-    distances = np.linspace(0.8, 2.2, npoints)
-    print(distances)
-
-    nangles = 5
-    angles = np.linspace(60, 180, nangles) / 180. * np.pi
-    print(angles)
-
-    frames = []
-    for ang in angles:
-        for i in range(npoints):
-            for j in range(i,npoints):
-                atoms = atoms_.copy()
-                # -- O1
-                atoms[1].position[0] = distances[i]
-                # -- O2
-                x, y = np.cos(ang)*distances[j], np.sin(ang)*distances[j]
-                atoms[2].position = [x,y,0.]
-                ...
-                frames.append(atoms)
-    print("nframes: ", len(frames))
-    write("CO2-hypercube.xyz", frames)
-    
     ...
