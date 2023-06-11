ed62cd3c2d2e3c06749123a6df7399e0c32f6ee0
hsulab <ahcigar@foxmail.com>
2023-06-11 14:35:50 -0400

    add placeholders for comparator

--- GDPy/comparator/__init__.py
+++ GDPy/comparator/__init__.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import inspect
-
 from ase.ga.ofp_comparator import OFPComparator
 from ase.ga.particle_comparator import NNMatComparator
 from ase.ga.standard_comparators import InteratomicDistanceComparator
 
-from GDPy.core.register import registers
+from ..core.register import registers
+from .graph import GraphComparator
 
 registers.comparator.register(OFPComparator)
 registers.comparator.register(NNMatComparator)
 registers.comparator.register(InteratomicDistanceComparator)
 
-@registers.comparator.register
-class GraphComparator:
+registers.comparator.register(GraphComparator)
 
-    def __init__(self) -> None:
-        ...
-
-#print(registers.comparator._dict)
-#comp = registers.get("comparator", "interatomic_distance")
-#print(comp)
-#args = inspect.getargspec(comp.__init__).args
-#print(args)
 
 if __name__ == "__main__":
     ...
