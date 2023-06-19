25417fccf4b1957785d6b9ebd3017bf590f63f56
hsulab <ahcigar@foxmail.com>
2023-06-19 18:57:22 -0400

    update packer and test

--- GDPy/builder/builder.py
+++ GDPy/builder/builder.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import copy
 import pathlib
 from typing import List, Callable
 
@@ -26,23 +27,16 @@ class StructureBuilder(AbstractNode):
     #: Standard debug function.
     _debug: Callable = print
 
-    def __init__(self, substrates=None, use_tags=False, directory="./", random_seed=None, *args, **kwargs):
+    def __init__(self, use_tags=False, directory="./", random_seed=None, *args, **kwargs):
         """"""
         super().__init__(directory=directory, random_seed=random_seed)
 
-        self.substrates = self._load_substrates(substrates)
         self.use_tags = use_tags
 
         return
     
-    def _load_substrates(self, inp_sub) -> List[Atoms]:
-        """"""
-        substrates = inp_sub # assume it is a List of Atoms
-
-        return substrates
-    
     @abc.abstractmethod
-    def run(self, *args, **kwargs) -> List[Atoms]:
+    def run(self, substrates=None, *args, **kwargs) -> List[Atoms]:
         """Generate structures based on rules."""
         if self.logger is not None:
             self.pfunc = self.logger.info
@@ -54,6 +48,36 @@ class StructureBuilder(AbstractNode):
         return
     
 StructureGenerator = StructureBuilder # compatibility
+
+
+class StructureModifier(StructureBuilder):
+
+    def __init__(self, substrates=None, *args, **kwargs):
+        """"""
+        super().__init__(*args, **kwargs)
+
+        self.substrates = self._load_substrates(substrates)
+
+        return
+
+    def _load_substrates(self, inp_sub) -> List[Atoms]:
+        """"""
+        substrates = inp_sub # assume it is a List of Atoms
+
+        return substrates
+
+    def run(self, substrates=None, *args, **kwargs):
+        """"""
+        super().run(*args, **kwargs)
+
+        # - load substrates at run
+        substrates_at_run = self._load_substrates(substrates)
+        if substrates_at_run is not None:
+            self.substrates = substrates_at_run
+        assert self.substrates is not None, "Substrates are not set neither at inp nor at run."
+        self.substrates = [copy.deepcopy(s) for s in self.substrates]
+
+        return
 
 if __name__ == "__main__":
     ...
