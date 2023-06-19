66aa799eed7ea32f5b5ce2ccdf862e5337d239b2
hsulab <ahcigar@foxmail.com>
2023-06-19 23:09:17 +0100

    add substrates as input

--- GDPy/builder/builder.py
+++ GDPy/builder/builder.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import abc
-from typing import List, Callable
-
 import pathlib
+from typing import List, Callable
 
 from ase import Atoms
 
@@ -21,11 +20,26 @@ class StructureBuilder(AbstractNode):
     logger = None #: Logger instance.
     pfunc: Callable = print #: Function for outputs.
 
-    def __init__(self, directory="./", random_seed=None, *args, **kwargs):
+    #: Standard print function.
+    _print: Callable = print
+
+    #: Standard debug function.
+    _debug: Callable = print
+
+    def __init__(self, substrates=None, use_tags=False, directory="./", random_seed=None, *args, **kwargs):
         """"""
         super().__init__(directory=directory, random_seed=random_seed)
 
+        self.substrates = self._load_substrates(substrates)
+        self.use_tags = use_tags
+
         return
+    
+    def _load_substrates(self, inp_sub) -> List[Atoms]:
+        """"""
+        substrates = inp_sub # assume it is a List of Atoms
+
+        return substrates
     
     @abc.abstractmethod
     def run(self, *args, **kwargs) -> List[Atoms]:
@@ -42,4 +56,4 @@ class StructureBuilder(AbstractNode):
 StructureGenerator = StructureBuilder # compatibility
 
 if __name__ == "__main__":
-    pass
+    ...
