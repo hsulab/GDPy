92a0c2e75c504689b8502293b5d96d180c7fa300
hsulab <ahcigar@foxmail.com>
2023-07-21 16:57:11 -0400

    accept nimages

--- GDPy/comparator/reaction.py
+++ GDPy/comparator/reaction.py
@@ -13,6 +13,8 @@ except Exception as e:
 
 from ase.utils.forcecurve import fit_raw
 
+from ..core.node import AbstractNode
+
 def get_forcefit(images):
     energies = np.array([a.get_potential_energy() for a in images])
     emin = np.min(energies)
@@ -27,10 +29,12 @@ def get_forcefit(images):
 
     return ff
 
-class ReactionComparator():
+class ReactionComparator(AbstractNode):
 
-    def __init__(self) -> None:
+    def __init__(self, nimages=7, *args, **kwargs) -> None:
         """"""
+
+        self.nimages = nimages
 
         return
     
@@ -43,6 +47,19 @@ class ReactionComparator():
         reference = reference.get_marked_structures()
         prediction = prediction.get_marked_structures()
 
+        nimages = self.nimages
+        for i in range(int(len(reference)/nimages)):
+            self._irun(
+                prediction[i*nimages:(i+1)*nimages], 
+                reference[i*nimages:(i+1)*nimages], 
+                f"{i}".zfill(4)+"."
+            )
+
+        self._report()
+
+        return
+
+    def _irun(self, prediction, reference, prefix):
         dene = prediction[0].get_potential_energy() - reference[0].get_potential_energy()
 
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
@@ -56,7 +73,7 @@ class ReactionComparator():
 
         ax.legend()
 
-        plt.savefig(self.directory/"neb.png")
+        plt.savefig(self.directory/f"{prefix}neb.png")
 
         return
     
@@ -65,6 +82,29 @@ class ReactionComparator():
         ff = get_forcefit(images)
         ax.scatter(ff.path, ff.energies-dene, label=label)
         ax.plot(ff.fit_path, ff.fit_energies-dene, "k-")
+
+        return
+    
+    def _report(self):
+        """"""
+        from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
+
+        story = []
+
+        # - find figures
+        nebfigures = list(self.directory.glob("*neb.png"))
+        nebfigures = sorted(nebfigures)
+        print(nebfigures)
+
+        for i, p in enumerate(nebfigures):
+            story.append(Paragraph(f"pathway {i}..."))
+            image = Image(p)
+            image.drawWidth = 240
+            image.drawHeight = 160
+            story.append(image)
+
+        doc = SimpleDocTemplate(str(self.directory/"report.pdf"))
+        doc.build(story)
 
         return
 
