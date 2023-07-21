b94fd274fbf12656ec2c0884c1b3704427dadb1b
hsulab <ahcigar@foxmail.com>
2023-07-21 16:56:19 -0400

    read_convergence when min reaches max_steps

--- GDPy/computation/driver.py
+++ GDPy/computation/driver.py
@@ -344,7 +344,7 @@ class AbstractDriver(abc.ABC):
         if nframes > 0:
             if self.setting.steps > 0:
                 step = traj_frames[-1].info["step"]
-                self._debug("nframes: ", nframes)
+                self._debug(f"nframes: {nframes}")
                 if self.setting.task == "min":
                     # NOTE: check geometric convergence (forces)...
                     #       some drivers does not store constraints in trajectories
@@ -356,14 +356,16 @@ class AbstractDriver(abc.ABC):
                         atoms._del_constraints()
                         atoms.set_constraint(FixAtoms(indices=frozen_indices))
                     maxfrc = np.max(np.fabs(atoms.get_forces(apply_constraint=True)))
-                    if maxfrc <= self.setting.fmax:
+                    if maxfrc <= self.setting.fmax or step+1 > self.setting.steps:
                         converged = True
-                    self._debug("MIN convergence: ", converged, f" {maxfrc} <=? {self.setting.fmax}")
+                    self._debug(
+                        f"MIN convergence: {converged} STEP: {step} >=? {self.setting.steps} MAXFRC: {maxfrc} <=? {self.setting.fmax}"
+                    )
                 elif self.setting.task == "md":
                     #print("steps: ", step, self.setting.steps)
                     if step+1 >= self.setting.steps: # step startswith 0
                         converged = True
-                    self._debug("MD convergence: ", converged)
+                    self._debug(f"MD convergence: {converged}")
                 else:
                     raise NotImplementedError("Unknown task in read_convergence.")
             else:
