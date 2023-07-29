6c6f8884e79cb5c859300244f12bca94ec2651ab
hsulab <ahcigar@foxmail.com>
2023-07-29 06:16:37 +0100

    add split_train_test method

--- GDPy/data/dataset.py
+++ GDPy/data/dataset.py
@@ -7,15 +7,18 @@ import pathlib
 from typing import List
 import warnings
 
+import numpy as np
+
 from ase import Atoms
 from ase.io import read, write
 
-from GDPy.core.register import registers
-from GDPy.core.variable import Variable
+from ..core.register import registers
+from ..core.variable import Variable
+from ..core.node import AbstractNode
 
 
 @registers.dataloader.register
-class XyzDataloader:
+class XyzDataloader(AbstractNode):
 
     name = "xyz"
 
@@ -30,9 +33,9 @@ class XyzDataloader:
 
     """
 
-    def __init__(self, dataset_path="./", batchsize=32, train_ratio=0.9, *args, **kwargs) -> None:
+    def __init__(self, dataset_path="./", batchsize=32, train_ratio=0.9, random_seed=None, *args, **kwargs) -> None:
         """"""
-        self.directory = pathlib.Path(dataset_path) # datset
+        super().__init__(directory=dataset_path, random_seed=random_seed)
 
         self.batchsize = batchsize
         self.train_ratio = train_ratio
@@ -62,19 +65,20 @@ class XyzDataloader:
                     ...
 
             return
+
         traverse_dirs(self.directory)
         data_dirs = sorted(data_dirs)
 
-        #print(len(data_dirs))
-        #for p in data_dirs:
-        #    print(str(p))
-
         return data_dirs
-    
-    def get_dataset(self):
-        """"""
-        data_dirs = self.load()
 
+    def split_train_test(self, reduce_system=False):
+        """Read structures and split them into train and test.
+
+        Args:
+            reduce_system: Whether merge different systems into one List.
+
+        """
+        data_dirs = self.load()
         self._print(data_dirs)
         self._print("\n--- auto data reader ---\n")
 
@@ -92,7 +96,6 @@ class XyzDataloader:
         for i, (cur_system, curr_batchsize) in enumerate(zip(data_dirs, batchsizes)):
             cur_system = pathlib.Path(cur_system)
             set_names.append(cur_system.name)
-            self._print(str(cur_system))
             self._print(f"System {cur_system.stem} Batchsize {curr_batchsize}\n")
             frames = [] # all frames in this subsystem
             subsystems = list(cur_system.glob("*.xyz"))
@@ -104,7 +107,65 @@ class XyzDataloader:
                 frames.extend(p_frames)
                 self._print(f"  subsystem: {p.name} number {p_nframes}\n")
 
-        return
+            # split dataset and get adjusted batchsize
+            # TODO: adjust batchsize of train and test separately
+            nframes = len(frames)
+            if nframes <= curr_batchsize:
+                if nframes == 1 or curr_batchsize == 1:
+                    new_batchsize = 1
+                else:
+                    new_batchsize = int(2**np.floor(np.log2(nframes)))
+                adjusted_batchsizes.append(new_batchsize)
+                # NOTE: use same train and test set
+                #       since they are very important structures...
+                train_index = list(range(nframes))
+                test_index = list(range(nframes))
+            else:
+                if nframes == 1 or curr_batchsize == 1:
+                    new_batchsize = 1
+                    train_index = list(range(nframes))
+                    test_index = list(range(nframes))
+                else:
+                    new_batchsize = curr_batchsize
+                    # - assure there is at least one batch for test
+                    #          and number of train frames is integer times of batchsize
+                    ntrain = int(np.floor(nframes * self.train_ratio / new_batchsize) * new_batchsize)
+                    train_index = self.rng.choice(nframes, ntrain, replace=False)
+                    test_index = [x for x in range(nframes) if x not in train_index]
+                adjusted_batchsizes.append(new_batchsize)
+
+            ntrain, ntest = len(train_index), len(test_index)
+            train_size.append(ntrain)
+            test_size.append(ntest)
+
+            self._print(f"    ntrain: {ntrain} ntest: {ntest} ntotal: {nframes} batchsize: {new_batchsize}\n")
+
+            curr_train_frames = [frames[train_i] for train_i in train_index]
+            curr_test_frames = [frames[test_i] for test_i in test_index]
+            if reduce_system:
+                # train
+                train_frames.extend(curr_train_frames)
+                n_train_frames = len(train_frames)
+
+                # test
+                test_frames.extend(curr_test_frames)
+                n_test_frames = len(test_frames)
+            else:
+                # train
+                train_frames.append(curr_train_frames)
+                n_train_frames = sum([len(x) for x in train_frames])
+
+                # test
+                test_frames.append(curr_test_frames)
+                n_test_frames = sum([len(x) for x in test_frames])
+            self._print(f"  Current Dataset -> ntrain: {n_train_frames} ntest: {n_test_frames}\n\n")
+
+        assert len(train_size) == len(test_size), "inconsistent train_size and test_size"
+        train_size = sum(train_size)
+        test_size = sum(test_size)
+        self._print(f"Total Dataset -> ntrain: {train_size} ntest: {test_size}\n")
+
+        return set_names, train_frames, test_frames, adjusted_batchsizes
     
     def transfer(self, frames: List[Atoms]):
         """Add structures into the dataset."""
@@ -142,7 +203,7 @@ class XyzDataloader:
         """"""
         dataset_params = {}
         dataset_params["name"] = self.name
-        dataset_params["dataset_path"] = str(self.directory)
+        dataset_params["dataset_path"] = str(self.directory.resolve())
         dataset_params["batchsize"] = self.batchsize
         dataset_params["train_ratio"] = self.train_ratio
 
