0ca6e2046b9ee09bc2db2ae0d6089bab60122805
hsulab <ahcigar@foxmail.com>
2024-02-08 21:41:54 -0500

    add sgp trainer

--- src/gdpx/potential/managers/gp/sgp.py
+++ src/gdpx/potential/managers/gp/sgp.py
@@ -4,6 +4,7 @@
 
 import collections
 import itertools
+from typing import List
 
 import numpy as np
 import scipy as sp 
@@ -15,6 +16,8 @@ from ase.io import read, write
 from ase.neighborlist import NeighborList, natural_cutoffs
 
 from itertools import permutations, product
+
+from gdpx.potential.trainer import AbstractTrainer
 
 def switch_function_value_and_grad(r, r_cut):
     """Switch function that smoothes distance to cutoff.
@@ -904,9 +907,9 @@ class SparseGaussianProcessTrainer():
         """"""
         # - read dataset
         #frames = read("./dft/O2.xyz", ":1")
-        #frames = read("./dft/Cu4.xyz", "1:2")
+        frames = read("./dft/Cu4.xyz", ":1")
         #frames = read("./dft/Cu13.xyz", ":")
-        frames = read("./md/cand0/traj.xyz", ":")
+        #frames = read("./md/Cu4/cand0/traj.xyz", ":")
         #frames = read("./Cu100/cand0/traj.xyz", ":5")
 
         energies = [a.get_potential_energy() for a in frames]
@@ -1266,13 +1269,57 @@ class SparseGaussianProcessTrainer():
         return
 
 
+class SGPTrainer(AbstractTrainer):
+
+    name = "sgp"
+
+    def __init__(
+        self, config: dict, type_list: List[str] = None, train_epochs: int = 200, 
+        directory=".", command="train", freeze_command="freeze", random_seed: int = None, 
+        *args, **kwargs
+    ) -> None:
+        """"""
+        super().__init__(
+            config, type_list, train_epochs, 
+            directory, command, freeze_command, 
+            random_seed, *args, **kwargs
+        )
+
+        return
+
+    @property
+    def frozen_name(self):
+        """"""
+        return f"{self.name}.pb"
+
+    def _resolve_freeze_command(self, *args, **kwargs):
+        return super()._resolve_freeze_command(*args, **kwargs)
+
+    def _resolve_train_command(self, *args, **kwargs):
+        """"""
+        command = self.command
+
+        return command
+    
+    def train(self, dataset, init_model=None, *args, **kwargs):
+        """"""
+        self._print("miaow")
+        sgp_trainer = SparseGaussianProcessTrainer()
+        #print("!!!!! BODY-2 !!!!!") 
+        sgp_trainer.run_b2()
+        #print("!!!!! BODY-3 !!!!!") 
+        #sgp_trainer.run_b3()
+        #print("!!!!! BODY-2+3 !!!!!") 
+        #sgp_trainer.run()
+
+        return
+    
+    def write_input(self, dataset, *args, **kwargs):
+        return super().write_input(dataset, *args, **kwargs)
+
+    def read_convergence(self) -> bool:
+        return super().read_convergence()
+
+
 if __name__ == "__main__":
-    """"""
-    sgp_trainer = SparseGaussianProcessTrainer()
-    #print("!!!!! BODY-2 !!!!!") 
-    #sgp_trainer.run_b2()
-    #print("!!!!! BODY-3 !!!!!") 
-    #sgp_trainer.run_b3()
-    #print("!!!!! BODY-2+3 !!!!!") 
-    sgp_trainer.run()
     ...
