c0a0e8e5b888b6fa8a801822c33c1e1fef5c0024
hsulab <ahcigar@foxmail.com>
2024-03-22 04:42:35 +0000

    format codes

--- src/gdpx/data/dataset.py
+++ src/gdpx/data/dataset.py
@@ -16,6 +16,7 @@ from ..core.register import registers
 from ..core.variable import Variable
 from ..core.node import AbstractNode
 
+
 def traverse_xyzdirs(wdir):
     """"""
     data_dirs = []
@@ -36,9 +37,7 @@ def traverse_xyzdirs(wdir):
     return data_dirs
 
 
-class AbstractDataloader(AbstractNode):
-
-    ...
+class AbstractDataloader(AbstractNode): ...
 
 
 @registers.dataloader.register
@@ -54,7 +53,15 @@ class XyzDataloader(AbstractDataloader):
 
     """
 
-    def __init__(self, dataset_path="./", batchsize=32, train_ratio=0.9, random_seed=None, *args, **kwargs) -> None:
+    def __init__(
+        self,
+        dataset_path="./",
+        batchsize=32,
+        train_ratio=0.9,
+        random_seed=None,
+        *args,
+        **kwargs,
+    ) -> None:
         """"""
         super().__init__(directory=dataset_path, random_seed=random_seed)
 
@@ -62,7 +69,7 @@ class XyzDataloader(AbstractDataloader):
         self.train_ratio = train_ratio
 
         return
-    
+
     def load(self) -> List[pathlib.Path]:
         """Load dataset.
 
@@ -76,7 +83,7 @@ class XyzDataloader(AbstractDataloader):
         data_dirs = sorted(data_dirs)
 
         return data_dirs
-    
+
     def load_frames(self, *args, **kwargs):
         """"""
         data_dirs = traverse_xyzdirs(self.directory)
@@ -97,14 +104,14 @@ class XyzDataloader(AbstractDataloader):
             self._debug(f"{i:>4d} {str(p)} -> {len(curr_frames)}")
             frames_list.append(curr_frames)
         self._debug(f"Number of frames: {nframes_tot}")
-        
+
         pairs = []
         for n, x in zip(names, frames_list):
             pairs.append([n, x])
 
         return pairs
 
-    def split_train_and_test(self, reduce_system: bool=False):
+    def split_train_and_test(self, reduce_system: bool = False):
         """Read structures and split them into train and test.
 
         Args:
@@ -118,22 +125,24 @@ class XyzDataloader(AbstractDataloader):
         batchsizes = self.batchsize
         nsystems = len(data_dirs)
         if isinstance(batchsizes, int):
-            batchsizes = [batchsizes]*nsystems
-        assert len(batchsizes) == nsystems, "Number of systems and batchsizes are inconsistent."
+            batchsizes = [batchsizes] * nsystems
+        assert (
+            len(batchsizes) == nsystems
+        ), "Number of systems and batchsizes are inconsistent."
 
         # read configurations
         set_names = []
         train_size, test_size = [], []
         train_frames, test_frames = [], []
-        adjusted_batchsizes = [] # auto-adjust batchsize based on nframes
+        adjusted_batchsizes = []  # auto-adjust batchsize based on nframes
         for i, (curr_system, curr_batchsize) in enumerate(zip(data_dirs, batchsizes)):
             curr_system = pathlib.Path(curr_system)
             set_name = "+".join(str(curr_system.relative_to(self.directory)).split("/"))
             set_names.append(set_name)
             self._print(f"System {set_name} Batchsize {curr_batchsize}")
-            frames = [] # all frames in this subsystem
+            frames = []  # all frames in this subsystem
             subsystems = list(curr_system.glob("*.xyz"))
-            subsystems.sort() # sort by alphabet
+            subsystems.sort()  # sort by alphabet
             for p in subsystems:
                 # read and split dataset
                 p_frames = read(p, ":")
@@ -148,7 +157,7 @@ class XyzDataloader(AbstractDataloader):
                 if nframes == 1 or curr_batchsize == 1:
                     new_batchsize = 1
                 else:
-                    new_batchsize = int(2**np.floor(np.log2(nframes)))
+                    new_batchsize = int(2 ** np.floor(np.log2(nframes)))
                 adjusted_batchsizes.append(new_batchsize)
                 # NOTE: use same train and test set
                 #       since they are very important structures...
@@ -163,7 +172,10 @@ class XyzDataloader(AbstractDataloader):
                     new_batchsize = curr_batchsize
                     # - assure there is at least one batch for test
                     #          and number of train frames is integer times of batchsize
-                    ntrain = int(np.floor(nframes * self.train_ratio / new_batchsize) * new_batchsize)
+                    ntrain = int(
+                        np.floor(nframes * self.train_ratio / new_batchsize)
+                        * new_batchsize
+                    )
                     train_index = self.rng.choice(nframes, ntrain, replace=False)
                     test_index = [x for x in range(nframes) if x not in train_index]
                 adjusted_batchsizes.append(new_batchsize)
@@ -172,7 +184,9 @@ class XyzDataloader(AbstractDataloader):
             train_size.append(ntrain)
             test_size.append(ntest)
 
-            self._print(f"    ntrain: {ntrain} ntest: {ntest} ntotal: {nframes} batchsize: {new_batchsize}")
+            self._print(
+                f"    ntrain: {ntrain} ntest: {ntest} ntotal: {nframes} batchsize: {new_batchsize}"
+            )
 
             curr_train_frames = [frames[train_i] for train_i in train_index]
             curr_test_frames = [frames[test_i] for test_i in test_index]
@@ -192,19 +206,23 @@ class XyzDataloader(AbstractDataloader):
                 # test
                 test_frames.append(curr_test_frames)
                 n_test_frames = sum([len(x) for x in test_frames])
-            self._print(f"  Current Dataset -> ntrain: {n_train_frames} ntest: {n_test_frames}")
+            self._print(
+                f"  Current Dataset -> ntrain: {n_train_frames} ntest: {n_test_frames}"
+            )
 
-        assert len(train_size) == len(test_size), "inconsistent train_size and test_size"
+        assert len(train_size) == len(
+            test_size
+        ), "inconsistent train_size and test_size"
         train_size = sum(train_size)
         test_size = sum(test_size)
         self._print(f"Total Dataset -> ntrain: {train_size} ntest: {test_size}")
 
         return set_names, train_frames, test_frames, adjusted_batchsizes
-    
+
     def transfer(self, frames: List[Atoms]):
         """Add structures into the dataset."""
         # - check chemical symbols
-        system_dict = {} # {formula: [indices]}
+        system_dict = {}  # {formula: [indices]}
 
         formulae = [a.get_chemical_formula() for a in frames]
         for k, v in itertools.groupby(enumerate(formulae), key=lambda x: x[1]):
@@ -213,10 +231,10 @@ class XyzDataloader(AbstractDataloader):
         # - transfer data
         for formula, curr_indices in system_dict.items():
             # -- TODO: check system type
-            system_type = self.system # currently, use user input one
+            system_type = self.system  # currently, use user input one
             # -- name = description+formula+system_type
             dirname = "-".join([self.directory.parent.name, formula, system_type])
-            target_subdir = self.target_dir/dirname
+            target_subdir = self.target_dir / dirname
             target_subdir.mkdir(parents=True, exist_ok=True)
 
             # -- save frames
@@ -224,7 +242,7 @@ class XyzDataloader(AbstractDataloader):
             curr_nframes = len(curr_frames)
 
             strname = self.version + ".xyz"
-            target_destination = self.target_dir/dirname/strname
+            target_destination = self.target_dir / dirname / strname
             if not target_destination.exists():
                 write(target_destination, curr_frames)
                 self._print(f"nframes {curr_nframes} -> {target_destination.name}")
@@ -232,7 +250,7 @@ class XyzDataloader(AbstractDataloader):
                 warnings.warn(f"{target_destination} exists.", UserWarning)
 
         return
-    
+
     def as_dict(self):
         """"""
         dataset_params = {}
