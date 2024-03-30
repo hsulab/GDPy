3c924d54a02685576f9a8c37b58b5db1472a4feb
hsulab <ahcigar@foxmail.com>
2024-03-30 20:53:44 +0000

    plumed can use list as input

--- src/gdpx/potential/managers/plumed/plumed.py
+++ src/gdpx/potential/managers/plumed/plumed.py
@@ -22,7 +22,7 @@ class PlumedManager(AbstractPotentialManager):
         """"""
 
         return
-    
+
     def register_calculator(self, calc_params: dict, *agrs, **kwargs) -> None:
         """"""
         super().register_calculator(calc_params, *agrs, **kwargs)
@@ -32,34 +32,44 @@ class PlumedManager(AbstractPotentialManager):
             try:
                 from .calculators.plumed2 import Plumed
             except:
-                raise ModuleNotFoundError("Please install py-plumed to use the ase interface.")
+                raise ModuleNotFoundError(
+                    "Please install py-plumed to use the ase interface."
+                )
 
             inp = pathlib.Path(calc_params.get("inp", "./plumed.inp"))
-            if inp.exists():
-                self.calc_params.update(inp=str(inp.absolute()))
+            if isinstance(inp, str) or isinstance(inp, pathlib.Path):
+                if inp.exists():
+                    input_lines = []
+                    with open(inp, "r") as fopen:
+                        lines = fopen.readlines()
+                        for line in lines:
+                            line = line.strip()
+                            if line and not line.startswith("#"):
+                                if "#" in line:
+                                    line = line[: line.index("#")]
+                                else:
+                                    line = line
+                                input_lines.append(line + "\n")
+                    self.calc_params.update(inp=input_lines)
+                else:
+                    raise FileNotFoundError(f"{inp} does not exist.")
+            elif isinstance(inp, list):
+                input_lines = inp
             else:
-                raise FileNotFoundError(f"{inp} does not exist.")
+                raise ValueError(f"Plumed input {inp} {type(inp)} is invalid.")
 
-            input_lines = []
-            with open(inp, "r") as fopen:
-                lines = fopen.readlines()
-                for line in lines:
-                    line = line.strip()
-                    if line and not line.startswith("#"):
-                        if "#" in line:
-                            line = line[:line.index("#")]
-                        else:
-                            line = line
-                        input_lines.append(line+"\n")
-            #print(f"input_lines: {input_lines}")
-                
-            kT = calc_params.get("kT", 1.)
+            kT = calc_params.get("kT", 1.0)
             use_charge = calc_params.get("use_charge", False)
             update_charge = calc_params.get("update_charge", False)
-            calc = Plumed(input=input_lines, kT=kT, use_charge=use_charge, update_charge=update_charge)
+            calc = Plumed(
+                input=input_lines,
+                kT=kT,
+                use_charge=use_charge,
+                update_charge=update_charge,
+            )
         else:
             ...
-        
+
         self.calc = calc
 
         return
