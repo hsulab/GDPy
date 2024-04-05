040623f5c7c11094b6392edcd50d24744eea7d18
hsulab <ahcigar@foxmail.com>
2024-04-05 14:46:13 -0400

    update pytest config

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+
+import numpy as np
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def change_test_dir(request):
+    os.chdir(request.fspath.dirname)
+    yield
+    os.chdir(request.config.invocation_params.dir)
+
+    return
+
+
+# NOTE: We assigan a random_seed and register all necessary modules here!!
+from gdpx import config
+config.GRNG = np.random.Generator(np.random.PCG64())
+
+from gdpx.core.register import import_all_modules_for_register
+import_all_modules_for_register()
+
+
+
+if __name__ == "__main__":
+    ...
