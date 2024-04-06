0ad2960efe18627d5c4709b477d5f29f0ea1ace1
hsulab <ahcigar@foxmail.com>
2024-04-06 04:52:05 +0100

    fix bug: vasp neb read_convergence

--- src/gdpx/reactor/string/string.py
+++ src/gdpx/reactor/string/string.py
@@ -9,7 +9,7 @@ import itertools
 import pathlib
 import re
 import shutil
-from typing import List
+from typing import Optional, Union, List
 
 import numpy as np
 
@@ -22,18 +22,6 @@ from ase.neb import interpolate, idpp_interpolate
 from .. import parse_constraint_info
 from ..reactor import AbstractReactor
 from ..utils import plot_bands, plot_mep
-
-
-def set_constraint(atoms, cons_text):
-    """"""
-    atoms._del_constraints()
-    mobile_indices, frozen_indices = parse_constraint_info(
-        atoms, cons_text, ignore_ase_constraints=True, ret_text=False
-    )
-    if frozen_indices:
-        atoms.set_constraint(FixAtoms(indices=frozen_indices))
-
-    return atoms
 
 
 @dataclasses.dataclass
@@ -50,7 +38,7 @@ class StringReactorSetting:
 
     #: Align IS and FS based on the mic.
     mic: bool = True
-    
+
     #: Optimiser.
     optimiser: str = "bfgs"
 
@@ -76,7 +64,7 @@ class StringReactorSetting:
     steps: int = 100
 
     #: FixAtoms.
-    constraint: str = None
+    constraint: Optional[str] = None
 
     #: Parameters that are used to update.
     _internals: dict = dataclasses.field(default_factory=dict)
@@ -89,7 +77,10 @@ class StringReactorSetting:
     def get_run_params(self):
         """"""
 
-        raise NotImplementedError(f"{self.__class__.__name__} has no function for run params.")
+        raise NotImplementedError(
+            f"{self.__class__.__name__} has no function for run params."
+        )
+
 
 class AbstractStringReactor(AbstractReactor):
 
@@ -98,9 +89,11 @@ class AbstractStringReactor(AbstractReactor):
     traj_name: str = "nebtraj.xyz"
 
     @AbstractReactor.directory.setter
-    def directory(self, directory_):
+    def directory(self, directory_: Union[str, pathlib.Path]):
         self._directory = pathlib.Path(directory_)
-        self.calc.directory = str(self.directory) # NOTE: avoid inconsistent in ASE
+        # avoid inconsistent in ASE
+        # the actual calc directory will be set in _irun
+        self.calc.directory = str(self.directory)
 
         return
 
@@ -141,7 +134,7 @@ class AbstractStringReactor(AbstractReactor):
                 self._irun(structures, ckpt_wdir=ckpt_wdir, *args, **kwargs)
             else:
                 self._debug(f"... converged @ {self.directory.name} ...")
-        
+
         self.calc.parameters = prev_params
         self.calc.reset()
 
@@ -149,11 +142,11 @@ class AbstractStringReactor(AbstractReactor):
         curr_band, converged = None, self.read_convergence()
         if converged:
             self._debug(f"... 2. converged @ {self.directory.name} ...")
-            band_frames = self.read_trajectory() # (nbands, nimages)
+            band_frames = self.read_trajectory()  # (nbands, nimages)
             if band_frames:
                 plot_mep(self.directory, band_frames[-1])
-                #plot_bands(self.directory, images, nimages=nimages_per_band)
-                write(self.directory/"temptraj.xyz", itertools.chain(*band_frames))
+                # plot_bands(self.directory, images, nimages=nimages_per_band)
+                write(self.directory / "temptraj.xyz", itertools.chain(*band_frames))
                 # --
                 curr_band = band_frames[-1]
                 energies = [a.get_potential_energy() for a in curr_band]
@@ -172,7 +165,7 @@ class AbstractStringReactor(AbstractReactor):
             self._debug(f"... 2. unconverged @ {self.directory.name} ...")
 
         return curr_band
-    
+
     @abc.abstractmethod
     def _irun(self, structures: List[Atoms], *args, **kwargs):
         """"""
@@ -180,13 +173,13 @@ class AbstractStringReactor(AbstractReactor):
         return
 
     def _verify_checkpoint(self, *args, **kwargs) -> bool:
-        """Check if the current directory has any valid outputs or it just created 
-            the input files.
+        """Check if the current directory has any valid outputs or it just created
+        the input files.
 
         """
 
         return self.directory.exists()
-    
+
     def _save_checkpoint(self, *args, **kwargs):
         """"""
         # - find previous runs...
@@ -194,7 +187,7 @@ class AbstractStringReactor(AbstractReactor):
         self._debug(f"prev_wdirs: {prev_wdirs}")
         curr_index = len(prev_wdirs)
 
-        curr_wdir = self.directory/f"{str(curr_index).zfill(4)}.run"
+        curr_wdir = self.directory / f"{str(curr_index).zfill(4)}.run"
         self._debug(f"curr_wdir: {curr_wdir}")
 
         # - backup files
@@ -202,9 +195,9 @@ class AbstractStringReactor(AbstractReactor):
         for x in self.directory.iterdir():
             if not re.match(r"[0-9]{4}\.run", x.name):
                 # NOTE: default is to move everything to the new folder
-                #if x.name in self.saved_fnames:
+                # if x.name in self.saved_fnames:
                 #    shutil.move(x, curr_wdir)
-                #else:
+                # else:
                 #    x.unlink()
                 shutil.move(x, curr_wdir)
             else:
@@ -212,13 +205,37 @@ class AbstractStringReactor(AbstractReactor):
 
         return curr_wdir
 
-    def _align_structures(self, structures, *args, **kwargs) -> List[Atoms]:
+    def _preprocess_constraints(self, atoms, cons_text: str) -> None:
         """"""
+        atoms._del_constraints()
+        mobile_indices, frozen_indices = parse_constraint_info(
+            atoms, cons_text, ignore_ase_constraints=True, ret_text=False
+        )
+        if frozen_indices:
+            atoms.set_constraint(FixAtoms(indices=frozen_indices))
+
+        return
+
+    def _align_structures(
+        self, structures: List[Atoms], run_params: dict, *args, **kwargs
+    ) -> List[Atoms]:
+        """Create a reaction pathway based on two structures.
+
+        Args:
+            structures: Two structures - Initial state and Final State.
+            run_params: We need thet latest `constraint` information.
+
+        Returns:
+            A List of Atoms structures.
+
+        """
         nstructures = len(structures)
         if nstructures == 2:
             # - check lattice consistency
             ini_atoms, fin_atoms = structures
-            c1, c2 = ini_atoms.get_cell(complete=True), fin_atoms.get_cell(complete=True)
+            c1, c2 = ini_atoms.get_cell(complete=True), fin_atoms.get_cell(
+                complete=True
+            )
             assert np.allclose(c1, c2), "Inconsistent unit cell..."
 
             # - align structures
@@ -232,18 +249,22 @@ class AbstractStringReactor(AbstractReactor):
             else:
                 self._print(f"disp: {np.linalg.norm(shifts)}")
 
-            ini_atoms = set_constraint(ini_atoms, self.setting.constraint)
-            fin_atoms = set_constraint(fin_atoms, self.setting.constraint)
+            cons_text = run_params.pop("constraint", None)
+            self._preprocess_constraints(ini_atoms, cons_text)
+            self._preprocess_constraints(fin_atoms, cons_text)
 
             # - find mep
             nimages = self.setting.nimages
             images = [ini_atoms]
-            images += [ini_atoms.copy() for i in range(nimages-2)]
+            images += [ini_atoms.copy() for i in range(nimages - 2)]
             images.append(fin_atoms)
 
             interpolate(
-                images=images, mic=False, interpolate_cell=False, 
-                use_scaled_coord=False, apply_constraint=False
+                images=images,
+                mic=False,
+                interpolate_cell=False,
+                use_scaled_coord=False,
+                apply_constraint=False,
             )
         else:
             self._print("Use a pre-defined pathway.")
@@ -261,19 +282,21 @@ class AbstractStringReactor(AbstractReactor):
         for w in prev_wdirs:
             curr_frames = self._read_a_single_trajectory(wdir=w)
             traj_list.append(curr_frames)
-        
+
         cache_nebtraj = self.directory / self.traj_name
         if cache_nebtraj.exists() and cache_nebtraj.stat().st_size != 0:
             traj_list.append(self._read_a_single_trajectory(wdir=self.directory))
-        
+
         # - concatenate
         traj_frames, ntrajs = [], len(traj_list)
         if ntrajs > 0:
             traj_frames.extend(traj_list[0])
             for i in range(1, ntrajs):
-                prev_end_band, curr_beg_band = traj_list[i-1][-1], traj_list[i][0]
+                prev_end_band, curr_beg_band = traj_list[i - 1][-1], traj_list[i][0]
                 for j, (a, b) in enumerate(zip(prev_end_band, curr_beg_band)):
-                    assert np.allclose(a.positions, b.positions), f"Traj {i-1} and traj {i} are not consecutive in positions."
+                    assert np.allclose(
+                        a.positions, b.positions
+                    ), f"Traj {i-1} and traj {i} are not consecutive in positions."
                 traj_frames.extend(traj_list[i][1:])
         else:
             ...
@@ -284,7 +307,7 @@ class AbstractStringReactor(AbstractReactor):
         """"""
         params = {}
 
-        #self._print(f"{self.setting.backend = }")
+        # self._print(f"{self.setting.backend = }")
 
         for k, v in dataclasses.asdict(self.setting).items():
             if not k.startswith("_"):
--- src/gdpx/reactor/string/vasp.py
+++ src/gdpx/reactor/string/vasp.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-import copy
-import itertools
 import dataclasses
 import os
-import pathlib
 import re
-import shutil
+import pathlib
 import traceback
 
 from typing import Union, List
@@ -16,23 +13,21 @@ from typing import Union, List
 import numpy as np
 
 from ase import Atoms
-from ase import units
 from ase.io import read, write
-from ase.calculators.cp2k import parse_input, InputSection
 from ase.calculators.singlepoint import SinglePointCalculator
+from ase.calculators.vasp import Vasp
 from ase.constraints import FixAtoms
-from ase.neb import NEB
 
-from .string import AbstractStringReactor, StringReactorSetting
 from .. import parse_constraint_info
+from .string import AbstractStringReactor, StringReactorSetting
 
 
 def run_vasp(name, command, directory):
-    """Run vasp from the command. 
-    
-    ASE Vasp does not treat restart of a MD simulation well. Therefore, we run 
+    """Run vasp from the command.
+
+    ASE Vasp does not treat restart of a MD simulation well. Therefore, we run
     directly from the command if INCAR aready exists.
-    
+
     """
     import subprocess
     from ase.calculators.calculator import EnvironmentError, CalculationFailed
@@ -51,12 +46,32 @@ def run_vasp(name, command, directory):
 
     if errorcode:
         path = os.path.abspath(directory)
-        msg = ('Calculator "{}" failed with command "{}" failed in '
-               '{} with error code {}'.format(name, command,
-                                              path, errorcode))
+        msg = (
+            'Calculator "{}" failed with command "{}" failed in '
+            "{} with error code {}".format(name, command, path, errorcode)
+        )
         raise CalculationFailed(msg)
 
     return
+
+
+def read_vaspout(
+    lines: List[str],
+) -> int:
+    """"""
+    pattern = re.compile("[0-9]+ F=")
+
+    steps = [0]
+    for line in lines:
+        m = pattern.match(line.strip())
+        if m:
+            step = int(m.group().split()[0])
+            steps.append(step)
+        else:
+            ...
+    # print(f"{steps =}")
+
+    return max(steps)
 
 
 @dataclasses.dataclass
@@ -71,19 +86,19 @@ class VaspStringReactorSetting(StringReactorSetting):
         """"""
         self._internals.update(
             # ---
-            ibrion = 3,
-            potim = 0,
-            isif = 2,
+            ibrion=3,
+            potim=0,
+            isif=2,
             # ---
-            lclimb = self.climb,
-            ichain = 0,
-            images = self.nimages-2,
-            iopt = 1,
-            spring = -5,
+            lclimb=self.climb,
+            ichain=0,
+            images=self.nimages - 2,
+            iopt=1,
+            spring=-5,
         )
 
         return
-    
+
     def get_run_params(self, *args, **kwargs):
         """"""
         # - convergence criteria
@@ -91,8 +106,9 @@ class VaspStringReactorSetting(StringReactorSetting):
         steps_ = kwargs.get("steps", self.steps)
 
         run_params = dict(
-            constraint = kwargs.get("constraint", self.constraint),
-            ediffg = fmax_*-1., nsw=steps_
+            constraint=kwargs.get("constraint", self.constraint),
+            ediffg=fmax_ * -1.0,
+            nsw=steps_,
         )
 
         return run_params
@@ -104,7 +120,15 @@ class VaspStringReactor(AbstractStringReactor):
 
     traj_name: str = "01/OUTCAR"
 
-    def __init__(self, calc=None, params={}, ignore_convergence=False, directory="./", *args, **kwargs) -> None:
+    def __init__(
+        self,
+        calc: Vasp = None,
+        params: dict = {},
+        ignore_convergence: bool = False,
+        directory: Union[str, pathlib.Path] = "./",
+        *args,
+        **kwargs,
+    ) -> None:
         """"""
         self.calc = calc
         if self.calc is not None:
@@ -119,10 +143,10 @@ class VaspStringReactor(AbstractStringReactor):
         self._debug(self.setting)
 
         return
-    
+
     def _verify_checkpoint(self):
-        """Check if the current directory has any valid outputs or it just created 
-            the input files.
+        """Check if the current directory has any valid outputs or it just created
+        the input files.
 
         """
         verified = super()._verify_checkpoint()
@@ -135,7 +159,7 @@ class VaspStringReactor(AbstractStringReactor):
             ...
 
         return verified
-    
+
     def _irun(self, structures: List[Atoms], ckpt_wdir=None, *args, **kwargs):
         """"""
         try:
@@ -143,52 +167,41 @@ class VaspStringReactor(AbstractStringReactor):
             run_params = self.setting.get_run_params(**kwargs)
             run_params.update(**self.setting.get_init_params())
 
-            if ckpt_wdir is None: # start from the scratch
-                images = self._align_structures(structures)
-                write(self.directory/"images.xyz", images)
+            if ckpt_wdir is None:  # start from the scratch
+                images = self._align_structures(structures, run_params)
+                write(self.directory / "images.xyz", images)
             else:
                 # - update structures
-                rep_dirs = sorted([x.name for x in sorted(self.directory.glob(r"[0-9][0-9]"))])
+                rep_dirs = sorted(
+                    [x.name for x in sorted(self.directory.glob(r"[0-9][0-9]"))]
+                )
 
                 frames_ = []
                 for x in rep_dirs[1:-1]:
-                    frames_.append(read(self.directory/x/"OUTCAR", ":"))
+                    frames_.append(read(self.directory / x / "OUTCAR", ":"))
                 nframes = min([len(x) for x in frames_])
                 assert nframes > 0, "At least one step finished before resume..."
-                intermediates_ = [x[nframes-1] for x in frames_]
+                intermediates_ = [x[nframes - 1] for x in frames_]
                 images = [structures[0]] + intermediates_ + [structures[-1]]
 
-                run_params.update(
-                    steps = self.setting.steps - nframes
-                )
-
-            # -- check constraint
-            atoms = images[0] # use the initial state
-            cons_text = run_params.pop("constraint", None)
-            mobile_indices, frozen_indices = parse_constraint_info(
-                atoms, cons_text=cons_text, ignore_ase_constraints=True, ret_text=False
-            )
-            if frozen_indices:
-                #atoms._del_constraints()
-                #atoms.set_constraint(FixAtoms(indices=frozen_indices))
-                frozen_indices = sorted(frozen_indices)
-                for a in images:
-                    a.set_constraint(FixAtoms(indices=frozen_indices))
+                run_params.update(steps=self.setting.steps - nframes)
 
             # -- add replica information
             for i, a in enumerate(images):
-                rep_dir = (self.directory/str(i).zfill(2))
-                rep_dir.mkdir() # TODO: exists?
-                write(rep_dir/"POSCAR", a)
+                rep_dir = self.directory / str(i).zfill(2)
+                rep_dir.mkdir()  # It has already been created when images are written.
+                write(rep_dir / "POSCAR", a)
 
             # - update input
             self.calc.set(**run_params)
+
+            atoms = images[0]
             atoms.calc = self.calc
 
             # - run calculation
             self.calc.write_input(atoms)
-            if (self.directory/"POSCAR").exists():
-                os.remove(self.directory/"POSCAR")
+            if (self.directory / "POSCAR").exists():
+                os.remove(self.directory / "POSCAR")
             run_vasp("vasp", atoms.calc.command, self.directory)
 
         except Exception as e:
@@ -196,21 +209,35 @@ class VaspStringReactor(AbstractStringReactor):
             self._debug(traceback.print_exc())
 
         return
-    
+
     def read_convergence(self, *args, **kwargs):
-        """"""
+        """Check whether vasp-neb is converged.
+
+        The convergence meets when either the required force is reached or
+        the maximum steps exceed.
+
+        """
         converged = super().read_convergence(*args, **kwargs)
 
-        with open(self.directory/"vasp.out", "r") as fopen:
-            lines = fopen.readlines()
-        
-        for line in lines:
-            if "reached required accuracy" in line:
+        self._print(f"{self.directory =}")
+        vaspout_fpath = self.directory / "vasp.out"
+        if vaspout_fpath.exists():
+            with open(self.directory / "vasp.out", "r") as fopen:
+                lines = fopen.readlines()
+
+            steps = read_vaspout(lines)
+            if steps >= self.setting.steps:
                 converged = True
-                break
+
+            for line in lines:
+                if "reached required accuracy" in line:
+                    converged = True
+                    break
+        else:
+            ...
 
         return converged
-    
+
     def _read_a_single_trajectory(self, wdir, *args, **kwargs):
         """
 
@@ -220,33 +247,41 @@ class VaspStringReactor(AbstractStringReactor):
         self._debug(f"***** read_trajectory *****")
         self._debug(f"{str(wdir)}")
 
-        images = read(wdir/"images.xyz", ":")
+        images = read(wdir / "images.xyz", ":")
         ini_atoms, fin_atoms = images[0], images[-1]
 
         # TODO: energy and forces of IS and FS?
         calc = SinglePointCalculator(
-            ini_atoms, energy=ini_atoms.info["energy"], forces=np.zeros((len(ini_atoms), 3))
+            ini_atoms,
+            energy=ini_atoms.info["energy"],
+            forces=np.zeros((len(ini_atoms), 3)),
         )
         ini_atoms.calc = calc
         calc = SinglePointCalculator(
-            fin_atoms, energy=fin_atoms.info["energy"], forces=np.zeros((len(fin_atoms), 3))
+            fin_atoms,
+            energy=fin_atoms.info["energy"],
+            forces=np.zeros((len(fin_atoms), 3)),
         )
         fin_atoms.calc = calc
 
         # - read OUTCARs
         frames_ = []
-        for i in range(1, self.setting.nimages-1):
-            curr_frames = read(wdir/f"{str(i).zfill(2)}"/"OUTCAR", ":")
+        for i in range(1, self.setting.nimages - 1):
+            curr_frames = read(wdir / f"{str(i).zfill(2)}" / "OUTCAR", ":")
             frames_.append(curr_frames)
 
-        # nframes may not consistent across replicas 
+        # nframes may not consistent across replicas
         # due to unfinished calculations
         nframes_list = [len(x) for x in frames_]
-        nsteps = min(nframes_list) 
+        nsteps = min(nframes_list)
 
         frames = []
         for i in range(nsteps):
-            curr_frames = [ini_atoms] + [frames_[j][i] for j in range(self.setting.nimages-2)] + [fin_atoms]
+            curr_frames = (
+                [ini_atoms]
+                + [frames_[j][i] for j in range(self.setting.nimages - 2)]
+                + [fin_atoms]
+            )
             frames.append(curr_frames)
 
         return frames
