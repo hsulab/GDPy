37d2c30eb54b3af36f7a5c9e4b90db32d7d438f5
hsulab <ahcigar@foxmail.com>
2024-04-23 02:16:40 -0400

    update grid worker

--- src/gdpx/worker/grid.py
+++ src/gdpx/worker/grid.py
@@ -6,7 +6,7 @@ import time
 import uuid
 import yaml
 
-from typing import List
+from typing import Optional, List
 
 from tinydb import Query, TinyDB
 
@@ -23,69 +23,97 @@ from ..scheduler.local import LocalScheduler
 
 class GridDriverBasedWorker(AbstractWorker):
 
-    def __init__(self, potter: List[AbstractPotentialManager], driver: AbstractDriver, scheduler: AbstractScheduler=LocalScheduler(), directory="./", *args, **kwargs) -> None:
+    def __init__(
+        self,
+        potters: List[AbstractPotentialManager],
+        drivers: List[AbstractDriver],
+        scheduler: AbstractScheduler = LocalScheduler(),
+        directory="./",
+        *args,
+        **kwargs,
+    ) -> None:
         """"""
-        super().__init__(directory)
+        super().__init__(directory, *args, **kwargs)
 
-        self.potter = potter # a List of potters
-        self.driver = driver
+        self.potters = potters
+
+        self.drivers = drivers
+
         self.scheduler = scheduler
 
         return
-    
-    def run(self, builder, *args, **kwargs):
-        """This worker accepts only a single structure."""
+
+    def _preprocess_structures(self, structures) -> List[Atoms]:
+        """Preprocess structures."""
+        if isinstance(structures, list):  # assume List[Atoms]
+            structures = structures
+        else:  # assume it is a builder
+            structures = structures.run()
+
+        return structures
+
+    def _spawn_computations(self):
+        """"""
+
+        return
+
+    def run(self, structures, *args, **kwargs):
+        """Run computations in batch.
+
+        The structure and the driver must be one-to-one
+
+        Args:
+            structures: A plain List[Atoms] or a builder.
+
+        """
         super().run(*args, **kwargs)
 
-        # - structures
-        if isinstance(builder, list): # assume List[Atoms]
-            frames = builder
-        else: # assume it is a builder
-            frames = builder.run()
-        nframes = len(frames)
-        assert len(frames) == 1, f"{self.__class__.__name__} accepts only a single structure."
+        # prepare batch
+        structures = self._preprocess_structures(structures)
+        num_structures = len(structures)
 
-        # - potentials
-        driver_dict = self.driver.as_dict()
-        drivers = [p.create_driver(driver_dict) for p in self.potter]
-        ndrivers = len(drivers)
+        drivers = self.drivers
+        num_drivers = len(drivers)
 
-        # -
+        assert num_structures == num_drivers
 
+        wdir_names = [f"cand{i}" for i in range(num_drivers)]
+
+        # ---
         uid = str(uuid.uuid1())
-        wdir_names = [f"pott{i}" for i in range(ndrivers)]
+        # wdir_names = [f"pott{i}" for i in range(ndrivers)]
         job_name = uid
 
         scheduler = self.scheduler
         if scheduler.name == "local":
             with CustomTimer(name="run-driver", func=self._print):
-                for wdir_name, driver in zip(wdir_names, drivers):
+                for wdir_name, atoms, driver in zip(wdir_names, structures, drivers):
                     driver.directory = self.directory / wdir_name
                     self._print(
                         f"{time.asctime( time.localtime(time.time()) )} {wdir_name} {driver.directory.name} is running..."
                     )
                     driver.reset()
-                    driver.run(frames[0], read_ckpt=True, extra_info=None)
+                    driver.run(atoms, read_ckpt=True, extra_info=None)
         else:
             worker_params = {}
             worker_params["use_single"] = True
             worker_params["driver"] = self.driver.as_dict()
             worker_params["potential"] = self.potter.as_dict()
 
-            with open(self.directory/f"worker-{uid}.yaml", "w") as fopen:
+            with open(self.directory / f"worker-{uid}.yaml", "w") as fopen:
                 yaml.dump(worker_params, fopen)
 
             # - save structures
-            dataset_path = str((self.directory/f"_gdp_inp.xyz").resolve())
+            dataset_path = str((self.directory / f"_gdp_inp.xyz").resolve())
             write(dataset_path, frames[0])
 
             # - save scheduler file
             jobscript_fname = f"run-{uid}.script"
             self.scheduler.job_name = job_name
-            self.scheduler.script = self.directory/jobscript_fname
+            self.scheduler.script = self.directory / jobscript_fname
 
             self.scheduler.user_commands = "gdp -p {} compute {}\n".format(
-                (self.directory/f"worker-{uid}.yaml").name, dataset_path
+                (self.directory / f"worker-{uid}.yaml").name, dataset_path
             )
 
             # - TODO: check whether params for scheduler is changed
@@ -97,16 +125,16 @@ class GridDriverBasedWorker(AbstractWorker):
 
         # - save this batch job to the database
         with TinyDB(
-            self.directory/f"_{self.scheduler.name}_jobs.json", indent=2
+            self.directory / f"_{self.scheduler.name}_jobs.json", indent=2
         ) as database:
             _ = database.insert(
                 dict(
-                    uid = uid,
-                    #md5 = identifier,
-                    gdir=job_name, 
-                    #group_number=ig, 
+                    uid=uid,
+                    # md5 = identifier,
+                    gdir=job_name,
+                    # group_number=ig,
                     wdir_names=wdir_names,
-                    queued=True
+                    queued=True,
                 )
             )
 
@@ -120,21 +148,21 @@ class GridDriverBasedWorker(AbstractWorker):
         running_jobs = self._get_running_jobs()
 
         with TinyDB(
-            self.directory/f"_{self.scheduler.name}_jobs.json", indent=2
+            self.directory / f"_{self.scheduler.name}_jobs.json", indent=2
         ) as database:
             for job_name in running_jobs:
                 doc_data = database.get(Query().gdir == job_name)
                 uid = doc_data["uid"]
 
                 self.scheduler.job_name = job_name
-                self.scheduler.script = self.directory/"train.script"
+                self.scheduler.script = self.directory / "train.script"
 
                 if self.scheduler.is_finished():
                     # -- check if the job finished properly
                     is_finished = False
                     wdir_names = doc_data["wdir_names"]
                     for x in wdir_names:
-                        wdir = self.directory/x
+                        wdir = self.directory / x
                         if not wdir.exists():
                             break
                         else:
@@ -149,16 +177,16 @@ class GridDriverBasedWorker(AbstractWorker):
                     else:
                         if resubmit:
                             jobid = self.scheduler.submit()
-                            self._print(f"{job_name} is re-submitted with JOBID {jobid}.")
+                            self._print(
+                                f"{job_name} is re-submitted with JOBID {jobid}."
+                            )
                 else:
                     self._print(f"{job_name} is running...")
 
         return
 
-    def retrieve(self, include_retrieved: bool=False, *args, **kwargs):
-        """Retrieve training results.
-
-        """
+    def retrieve(self, include_retrieved: bool = False, *args, **kwargs):
+        """Retrieve training results."""
         self.inspect(*args, **kwargs)
         self._debug(f"~~~{self.__class__.__name__}+retrieve")
 
@@ -169,12 +197,12 @@ class GridDriverBasedWorker(AbstractWorker):
             unretrieved_jobs = self._get_finished_jobs()
 
         with TinyDB(
-            self.directory/f"_{self.scheduler.name}_jobs.json", indent=2
+            self.directory / f"_{self.scheduler.name}_jobs.json", indent=2
         ) as database:
             for job_name in unretrieved_jobs:
                 doc_data = database.get(Query().gdir == job_name)
                 unretrieved_wdirs_.extend(
-                    (self.directory/w).resolve() for w in doc_data["wdir_names"]
+                    (self.directory / w).resolve() for w in doc_data["wdir_names"]
                 )
             unretrieved_wdirs = unretrieved_wdirs_
 
@@ -191,13 +219,13 @@ class GridDriverBasedWorker(AbstractWorker):
                 database.update({"retrieved": True}, doc_ids=[doc_data.doc_id])
 
         return results
-    
+
     def as_dict(self) -> dict:
         """"""
         params = super().as_dict()
 
         return params
-    
+
 
 if __name__ == "__main__":
     ...
