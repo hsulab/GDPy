579e78e299293e90ef959082fa4af0b93072ba5c
hsulab <ahcigar@foxmail.com>
2024-04-28 23:06:51 -0400

    update AseDriver tests

--- tests/computation/asedriver/test_asedriver.py
+++ tests/computation/asedriver/test_asedriver.py
@@ -4,226 +4,150 @@
 
 import copy
 import logging
-import pytest
+import os
+import pathlib
 import tempfile
 
+import pytest
+import yaml
 from ase.io import read, write
 
 from gdpx import config
-from gdpx.core.register import import_all_modules_for_register
-from gdpx.worker.interface import ComputerVariable
+from gdpx.cli.compute import convert_config_to_potter
 
-
-import_all_modules_for_register()
-config.logger.setLevel(logging.DEBUG)
-
-
-@pytest.fixture
-def emt_config():
-    """"""
-    params = dict(
-        potential = dict(
-            name = "emt",
-            params = dict(
-                backend = "ase"
-            )
-        ),
-        driver = dict(
-            backend = "ase"
-        )
-    )
-
-    return params
-
-@pytest.fixture
-def emt_md_config():
-    """"""
-    params = dict(
-        potential = dict(
-            name = "emt",
-            params = dict(
-                backend = "ase"
-            )
-        ),
-        driver = dict(
-            backend = "ase",
-            ignore_convergence = True,
-            task = "md",
-            init = dict(
-                velocity_seed = 1112,
-                dump_period = 1
-            ),
-            run = dict(
-                steps = 10
-            )
-        )
-    )
-
-    return params
-
-@pytest.fixture
-def emt_min_config():
-    """"""
-    params = dict(
-        potential = dict(
-            name = "emt",
-            params = dict(
-                backend = "ase"
-            )
-        ),
-        driver = dict(
-            backend = "ase",
-            ignore_convergence = True,
-            task = "min",
-            run = dict(
-                fmax = 0.10,
-                steps = 400
-            )
-        )
-    )
-
-    return params
-
-
-def test_empty(emt_config):
-    """"""
-    worker = ComputerVariable(emt_config["potential"], emt_config["driver"]).value[0]
-
-    driver = worker.driver
-    driver.directory = "./assets/empty_driver"
-
-    converged = driver.read_convergence()
-
-    assert not converged
-
-def test_broken_spc(emt_config):
-    """"""
-    worker = ComputerVariable(emt_config["potential"], emt_config["driver"]).value[0]
-
-    driver = worker.driver
-    driver.directory = "./assets/broken_ase_spc"
-
-    #print("broken: ", driver.read_convergence())
-
-    #atoms = read("../assets/Cu-fcc-s111p22.xyz")
-    #new_atoms = driver.run(atoms, read_ckpt=True)
-    #print(driver.read_convergence())
-
-    converged = driver.read_convergence()
-
-    assert not converged
-
-def test_finished_spc(emt_config):
-    """"""
-    worker = ComputerVariable(emt_config["potential"], emt_config["driver"]).value[0]
-
-    driver = worker.driver
-    driver.directory = "./assets/finished_ase_spc"
-
-    converged = driver.read_convergence()
-
-    #atoms = read("../assets/Cu-fcc-s111p22.xyz")
-    #new_atoms = driver.run(atoms, read_ckpt=True)
-    #print(driver.read_convergence())
-
-    assert converged
-
-
-def test_finished_md(emt_md_config):
-    """"""
-    with tempfile.TemporaryDirectory() as tmpdir:
-        #tmpdir = "./xxx"
-        # - run 10 steps
-        config = copy.deepcopy(emt_md_config)
-        worker = ComputerVariable(config["potential"], config["driver"]).value[0]
-
-        driver = worker.driver
-        driver.directory = tmpdir
-
-        converged = driver.read_convergence()
-        print("before: ", converged)
-
-        atoms = read("../assets/Cu-fcc-s111p22.xyz")
-
-        new_atoms = driver.run(atoms, read_ckpt=True)
-        print(driver.read_convergence())
-
-    return
-
-def test_finished_min(emt_min_config):
-    """"""
-    config = emt_min_config
-    with tempfile.TemporaryDirectory() as tmpdir:
-        #tmpdir = "./xxx"
-        # - run 10 steps
-        config = copy.deepcopy(config)
-        worker = ComputerVariable(config["potential"], config["driver"]).value[0]
-
-        driver = worker.driver
-        driver.directory = tmpdir
-
-        converged = driver.read_convergence()
-        print("before: ", converged)
-
-        atoms = read("../assets/Cu-fcc-s111p22.xyz")
-
-        new_atoms = driver.run(atoms, read_ckpt=True)
-        print(driver.read_convergence())
-
-    return
-
-
-#  TODO: Add tests for MIN with various dump_period
-#        AseMin must dump the last frame of the minisation trajectory
+#  NOTE: AseDriver must dump the last frame of the trajectory
 #        For example, a 9-frame trajectory with a dump period of 3,
 #        it must dump step 0, 3, 6, 8 even the last step is not the times of 3.
 #        dump_period = 5, it dumps 0, 5, 8.
 
 
-@pytest.mark.parametrize("dump_period,nframes", [(1, 21), (3, 7), (5, 5)])
-def test_restart_md(emt_md_config, dump_period, nframes):
+@pytest.mark.parametrize("dump_period,num_frames", [(1, 24), (3, 9), (5, 6)])
+def test_ase_min(dump_period, num_frames):
     """"""
-    # - run 10 steps
-    with tempfile.TemporaryDirectory() as tmpdir:
-        #tmpdir = "./xxx"
-        config = copy.deepcopy(emt_md_config)
+    structures = read("./assets/Pd38_oct.xyz", ":")
+
+    with open("./assets/emtmin.yaml", "r") as fopen:
+        worker_params = yaml.safe_load(fopen)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # tmpdirname = f"_xxx_{num_frames}"
+
+        config = copy.deepcopy(worker_params)
         config["driver"]["init"]["dump_period"] = dump_period
-        worker = ComputerVariable(config["potential"], config["driver"]).value[0]
 
-        driver = worker.driver
-        driver.directory = tmpdir
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
 
-        converged = driver.read_convergence()
-        print("before: ", converged)
+        worker.run(structures)
+        results = worker.retrieve(include_retrieved=True)
 
-        atoms = read("../assets/Cu-fcc-s111p22.xyz")
+    assert len(results[-1]) == num_frames
 
-        new_atoms = driver.run(atoms, read_ckpt=True)
-        print("10 steps: ", driver.read_convergence())
+    return
 
-        # - run extra 10 steps within the same dir
-        config = copy.deepcopy(emt_md_config)
+
+@pytest.mark.parametrize("dump_period,num_frames", [(1, 24), (3, 9), (5, 6)])
+def test_ase_nvt(dump_period, num_frames):
+    """"""
+    structures = read("./assets/Pd38_oct.xyz", ":")
+
+    with open("./assets/emtnvt.yaml", "r") as fopen:
+        worker_params = yaml.safe_load(fopen)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # tmpdirname = f"_xxx_{num_frames}"
+
+        config = copy.deepcopy(worker_params)
         config["driver"]["init"]["dump_period"] = dump_period
-        config["driver"]["run"]["steps"] = 20
-        print("new: ", config)
 
-        worker = ComputerVariable(config["potential"], config["driver"]).value[0]
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
 
-        driver = worker.driver
-        driver.directory = tmpdir
+        worker.run(structures)
+        results = worker.retrieve(include_retrieved=True)
 
-        converged = driver.read_convergence()
-        print("restart: ", converged)
+    assert len(results[-1]) == num_frames
 
-        atoms = read("../assets/Cu-fcc-s111p22.xyz")
+    return
 
-        new_atoms = driver.run(atoms, read_ckpt=True)
-        print(driver.read_convergence())
 
-        traj = driver.read_trajectory()
-        #print(traj[5].get_kinetic_energy())
+@pytest.mark.parametrize("dump_period,ckpt_period", [(1, 1), (1, 3), (3, 5), (3, 7)])
+# @pytest.mark.parametrize("dump_period,ckpt_period", [(1, 1)])
+def test_ase_min_restart(dump_period, ckpt_period):
+    """"""
+    structures = read("./assets/Pd38_oct.xyz", ":")
 
-        assert len(traj) == nframes
+    with open("./assets/emtmin.yaml", "r") as fopen:
+        worker_params = yaml.safe_load(fopen)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # tmpdirname = f"_xxx_{dump_period}_{ckpt_period}"
+        tmpdirname = pathlib.Path(tmpdirname)
+
+        # run first 24 steps
+        config = copy.deepcopy(worker_params)
+        config["driver"]["init"]["dump_period"] = dump_period
+        config["driver"]["init"]["ckpt_period"] = ckpt_period
+
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
+        worker.run(structures)
+
+        #
+        os.remove(tmpdirname / "_local_jobs.json")
+        config["driver"]["run"]["steps"] = 37
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
+        worker.run(structures)
+
+        results = worker.retrieve(include_retrieved=True)
+
+    step = results[-1][-1].info["step"]
+    energy = results[-1][-1].get_potential_energy()
+
+    assert step == 37
+    # assert energy == pytest.approx(16.641920)
+
+    return
+
+
+@pytest.mark.parametrize("dump_period,ckpt_period", [(1, 1), (1, 3), (3, 5), (3, 7)])
+def test_ase_nvt_restart(dump_period, ckpt_period):
+    """"""
+    structures = read("./assets/Pd38_oct.xyz", ":")
+
+    with open("./assets/emtnvt.yaml", "r") as fopen:
+        worker_params = yaml.safe_load(fopen)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # tmpdirname = f"_xxx_{dump_period}_{ckpt_period}"
+        tmpdirname = pathlib.Path(tmpdirname)
+
+        # run first 24 steps
+        config = copy.deepcopy(worker_params)
+        config["driver"]["init"]["dump_period"] = dump_period
+        config["driver"]["init"]["ckpt_period"] = ckpt_period
+
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
+        worker.run(structures)
+
+        #
+        os.remove(tmpdirname / "_local_jobs.json")
+        config["driver"]["run"]["steps"] = 37
+        worker = convert_config_to_potter(config)[0]
+        worker.directory = tmpdirname
+        worker.run(structures)
+
+        results = worker.retrieve(include_retrieved=True)
+
+    step = results[-1][-1].info["step"]
+    energy = results[-1][-1].get_potential_energy()
+
+    assert step == 37
+    assert energy == pytest.approx(16.641920)
+
+    return
 
 
 if __name__ == "__main__":
