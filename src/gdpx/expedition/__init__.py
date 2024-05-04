0a74b2bd775ce0cd85f667c828f0985b1837ad92
hsulab <ahcigar@foxmail.com>
2024-05-03 23:41:15 -0400

    use molecule object

--- src/gdpx/expedition/__init__.py
+++ src/gdpx/expedition/__init__.py
@@ -8,7 +8,7 @@ from ..builder.group import create_a_group, create_a_molecule_group
 from ..builder.utils import convert_string_to_atoms
 from ..core.register import registers
 from ..data.array import AtomsNDArray
-from ..graph.molecule import find_molecules
+from ..graph.molecule import MolecularAdsorbate, find_molecules
 from ..potential.interface import create_mixer
 from ..utils.command import convert_indices, dict2str
 from ..utils.strconv import str2array
