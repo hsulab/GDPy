de83bad0fc2d342768b8e73bcb358257b573a0c8
jyxu <ahcigar@foxmail.com>
2024-08-22 16:28:10 -0600

    move get_tags_per_species to utils

--- src/gdpx/builder/region.py
+++ src/gdpx/builder/region.py
@@ -14,38 +14,8 @@ from ase import Atoms
 from ase import data
 
 from . import registers
+from .utils import get_tags_per_species
 
-
-def get_tags_per_species(atoms: Atoms) -> Mapping[str,Mapping[int,List[int]]]:
-    """Get tags per species.
-
-    Example:
-
-        .. code-block:: python
-
-            >>> atoms = Atoms("PtPtPtCOCO")
-            >>> tags = [0, 0, 0, 1, 1, 2, 2]
-            >>> atoms.set_tags(tags)
-            >>> get_tags_per_species(atoms)
-            >>> {'Pt3': {0: [0,1,2]}, 'CO': {1: [3,4], 2: [5,6]}}
-
-    """
-
-    tags = atoms.get_tags() # default is all zero
-
-    tags_dict = {} # species -> tag list
-    for key, group in itertools.groupby(enumerate(tags), key=lambda x:x[1]):
-        cur_indices = [x[0] for x in group]
-        #print(key, " :", cur_indices)
-        cur_atoms = atoms[cur_indices]
-        formula = cur_atoms.get_chemical_formula()
-        #print(formula)
-        #print(key)
-        if formula not in tags_dict:
-            tags_dict[formula] = []
-        tags_dict[formula].append([key, cur_indices])
-
-    return tags_dict
 
 
 class Region(abc.ABC):
