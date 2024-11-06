d72f4acafe13092ebcda597de135654b0d102acb
hsulab <ahcigar@foxmail.com>
2024-11-06 11:26:15 -0500

    support density scale

--- src/gdpx/builder/scale.py
+++ src/gdpx/builder/scale.py
@@ -7,10 +7,21 @@ from typing import Optional, List
 import numpy as np
 
 from ase import Atoms
+from ase import units
 from ase.io import read, write
 from ase.build import make_supercell
 
 from .builder import StructureModifier
+
+
+def compute_density(atoms: Atoms):
+    """Compute density in [g/cm^3]."""
+    mass = np.sum(atoms.get_masses())
+    
+    volumes = atoms.get_volume()
+    density = (mass/units._Nav) / (volumes*1e-24)  # a.u./A^3 -> g/cm^3
+
+    return density
 
 
 class ScaleModifier(StructureModifier):
@@ -18,12 +29,14 @@ class ScaleModifier(StructureModifier):
     """Make a supercell and scale its box.
     """
 
-    def __init__(self, supercell: Optional[List[int]]=None, cubic: bool= False, substrates=None, *args, **kwargs):
+    def __init__(self, supercell: Optional[List[int]]=None, cubic: bool= False, density: float=-1.0, substrates=None, *args, **kwargs):
         """"""
         super().__init__(substrates=substrates, *args, **kwargs)
 
         self.supercell = np.array(supercell if supercell else [1,1,1])
         self.cubic = cubic
+        
+        self.density = density  # [g/cm^3]
 
         return
     
@@ -44,6 +57,12 @@ class ScaleModifier(StructureModifier):
         if self.cubic:  # TODO: This works only for an ortho box.
             new_cell = np.diag([np.max(new_atoms.cell.cellpar()[:3])]*3)
             new_atoms.set_cell(new_cell, scale_atoms=True)
+        if self.density > 0.:
+            prev_density = compute_density(new_atoms)
+            new_cell = new_atoms.get_cell(complete=True)/((self.density/prev_density)**(1/3))
+            new_atoms.set_cell(new_cell, scale_atoms=True)
+            curr_density = compute_density(new_atoms)
+            assert np.isclose(self.density, curr_density), f"New density `{curr_density}` does not equal `{self.density}`."
 
         return [new_atoms]
     
