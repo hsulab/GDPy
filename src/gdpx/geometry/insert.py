fd541afafe11a4792028a79adfb71fb2d57f4618
hsulab <ahcigar@foxmail.com>
2024-11-27 17:07:46 -0500

    move translate_then_rotate to a separate file

--- src/gdpx/geometry/insert.py
+++ src/gdpx/geometry/insert.py
@@ -10,26 +10,8 @@ import numpy as np
 from ase import Atoms
 from ase.geometry import find_mic
 
+from .particle import translate_then_rotate
 from .spatial import check_atomic_distances, check_pair_distances
-
-
-def translate_then_rotate(atoms, position, use_com: bool, rng):
-    """"""
-    num_atoms = len(atoms)
-    if not use_com:
-        center = np.mean(atoms.positions, axis=0)
-    else:
-        center = atoms.get_center_of_mass()
-
-    # Translate
-    atoms.translate(position - center)
-
-    # Rotate
-    if num_atoms > 1:
-        phi, theta, psi = 360 * rng.uniform(0, 1, 3)
-        atoms.euler_rotate(phi=phi, theta=0.5 * theta, psi=psi, center=position)
-
-    return atoms
 
 
 def insert_fragments_by_step(
---
+++ src/gdpx/geometry/particle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+from ase import Atoms
+
+
+def translate_then_rotate(atoms: Atoms, position, use_com: bool, rng):
+    """"""
+    num_atoms = len(atoms)
+    if not use_com:
+        center = np.mean(atoms.positions, axis=0)
+    else:
+        center = atoms.get_center_of_mass()
+
+    # Translate
+    atoms.translate(position - center)
+
+    # Rotate
+    if num_atoms > 1:
+        phi, theta, psi = 360 * rng.uniform(0, 1, 3)
+        atoms.euler_rotate(phi=phi, theta=0.5 * theta, psi=psi, center=position)
+
+    return atoms
+
+
+if __name__ == "__main__":
+    ...
