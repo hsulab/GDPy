ae7f39b0f55f1c4df6edc052a8b3642941989ef0
hsulab <ahcigar@foxmail.com>
2025-01-07 00:24:54 -0500

    improve: reaction uses BaseComponent

--- src/gdpx/comparator/reaction.py
+++ src/gdpx/comparator/reaction.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 try:
     plt.style.use("presentation")
 except Exception as e:
@@ -11,38 +12,41 @@ except Exception as e:
 
 try:
     USE_REPORTLAB = 1
-    from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
+    from reportlab.platypus import Image, Paragraph, SimpleDocTemplate
 except:
     USE_REPORTLAB = 0
 
 
+from ase import Atoms
 from ase.utils.forcecurve import fit_raw
 
-from ..core.node import AbstractNode
+from gdpx.core.component import BaseComponent
 
-def get_forcefit(images):
+
+def get_forcefit(images: list[Atoms]):
     energies = np.array([a.get_potential_energy() for a in images])
     emin = np.min(energies)
     forces = [a.get_forces(apply_constraint=True) for a in images]
     positions = [a.positions for a in images]
     cell, pbc = images[0].get_cell(complete=True), True
-    #energies -= emin
-    #rxn_coords = compute_rxn_coords(images)
-    #ax.scatter(rxn_coords, energies, label="dp")
-    ff = fit_raw(energies, forces, positions, cell, pbc) # ForceFit
-    #print(ff)
+    # energies -= emin
+    # rxn_coords = compute_rxn_coords(images)
+    # ax.scatter(rxn_coords, energies, label="dp")
+    ff = fit_raw(energies, forces, positions, cell, pbc)  # ForceFit
+    # print(ff)
 
     return ff
 
-class ReactionComparator(AbstractNode):
 
-    def __init__(self, nimages=7, *args, **kwargs) -> None:
+class ReactionComparator(BaseComponent):
+
+    def __init__(self, nimages=7) -> None:
         """"""
 
         self.nimages = nimages
 
         return
-    
+
     def run(self, prediction, reference):
         """"""
         # - input shape should be (2, ?, ?nimages_per_band)?
@@ -53,14 +57,14 @@ class ReactionComparator(AbstractNode):
         prediction = prediction.get_marked_structures()
 
         nimages = self.nimages
-        for i in range(int(len(reference)/nimages)):
+        for i in range(int(len(reference) / nimages)):
             self._irun(
-                prediction[i*nimages:(i+1)*nimages], 
-                reference[i*nimages:(i+1)*nimages], 
-                f"{i}".zfill(4)+"."
+                prediction[i * nimages : (i + 1) * nimages],
+                reference[i * nimages : (i + 1) * nimages],
+                f"{i}".zfill(4) + ".",
             )
 
-        if USE_REPORTLAB: 
+        if USE_REPORTLAB:
             self._report()
         else:
             self._print("Please install `reportlab` to report comparison.")
@@ -68,7 +72,9 @@ class ReactionComparator(AbstractNode):
         return
 
     def _irun(self, prediction, reference, prefix):
-        dene = prediction[0].get_potential_energy() - reference[0].get_potential_energy()
+        dene = (
+            prediction[0].get_potential_energy() - reference[0].get_potential_energy()
+        )
 
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
         plt.suptitle("Nudged Elastic Band Calculation")
@@ -77,22 +83,22 @@ class ReactionComparator(AbstractNode):
         ax.set_ylabel("Potential Energy [eV]")
 
         self._add_axis(ax, reference, dene, "dft")
-        self._add_axis(ax, prediction, 0., "dp")
+        self._add_axis(ax, prediction, 0.0, "dp")
 
         ax.legend()
 
-        plt.savefig(self.directory/f"{prefix}neb.png")
+        plt.savefig(self.directory / f"{prefix}neb.png")
 
         return
-    
+
     def _add_axis(self, ax, images, dene: float, label):
         """"""
         ff = get_forcefit(images)
-        ax.scatter(ff.path, ff.energies-dene, label=label)
-        ax.plot(ff.fit_path, ff.fit_energies-dene, "k-")
+        ax.scatter(ff.path, ff.energies - dene, label=label)
+        ax.plot(ff.fit_path, ff.fit_energies - dene, "k-")
 
         return
-    
+
     def _report(self):
         """"""
         story = []
@@ -109,7 +115,7 @@ class ReactionComparator(AbstractNode):
             image.drawHeight = 160
             story.append(image)
 
-        doc = SimpleDocTemplate(str(self.directory/"report.pdf"))
+        doc = SimpleDocTemplate(str(self.directory / "report.pdf"))
         doc.build(story)
 
         return
