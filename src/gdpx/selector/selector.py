21abc949a8bc446e833ad794e19a8665651a51f4
hsulab <ahcigar@foxmail.com>
2025-01-07 00:04:39 -0500

    improve: selector uses BaseComponent

--- src/gdpx/selector/selector.py
+++ src/gdpx/selector/selector.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import abc 
+
+import abc
 import copy
 import itertools
 import pathlib
-from typing import Union, List, Callable, NoReturn, Optional
+from typing import Optional, Union
 
 import numpy as np
-
 from ase import Atoms
 
-from ..core.node import AbstractNode
-from ..worker.drive import DriverBasedWorker
-from ..data.array import AtomsNDArray
+from gdpx.core.component import BaseComponent
 
+from ..data.array import AtomsNDArray
+from ..worker.drive import DriverBasedWorker
 
 """Define an AbstractSelector that is the base class of any selector.
 """
 
-def save_cache(fpath, data, random_seed: int=None):
+
+def save_cache(fpath, data, random_seed: Optional[int] = None):
     """"""
-    header = ("#{:>11s}  {:>8s}  {:>8s}  {:>8s}  "+"{:>12s}"*4+"\n").format(
+    header = ("#{:>11s}  {:>8s}  {:>8s}  {:>8s}  " + "{:>12s}" * 4 + "\n").format(
         *"index confid step natoms ene aene maxfrc score".split()
     )
     footer = f"random_seed {random_seed}"
 
     content = header
     for x in data:
-        content += ("{:>12s}  {:>8d}  {:>8d}  {:>8d}  "+"{:>12.4f}"*4+"\n").format(*x)
+        content += (
+            "{:>12s}  {:>8d}  {:>8d}  {:>8d}  " + "{:>12.4f}" * 4 + "\n"
+        ).format(*x)
     content += footer
 
     with open(fpath, "w") as fopen:
@@ -36,7 +39,8 @@ def save_cache(fpath, data, random_seed: int=None):
 
     return
 
-def load_cache(fpath, random_seed: int=None):
+
+def load_cache(fpath, random_seed: Optional[int] = None):
     """"""
     with open(fpath, "r") as fopen:
         lines = fopen.readlines()
@@ -45,18 +49,18 @@ def load_cache(fpath, random_seed: int=None):
     header = lines[0]
 
     # - data
-    data = lines[1:-1] # TODO: test empty data
+    data = lines[1:-1]  # TODO: test empty data
 
     raw_markers = []
     if data:
         # NOTE: new_markers looks like [(0,1),(0,2),(1,0)]
         #       If no structures are selected, the info file should only contain
         #       the header and the footer
-        new_markers =[
+        new_markers = [
             [int(x) for x in (d.strip().split()[0]).split(",")] for d in data
         ]
-        #new_markers = []
-        #for d in data:
+        # new_markers = []
+        # for d in data:
         #    curr_marker = []
         #    for x in d.strip().split()[0].split(","):
         #        if x.isdigit(): # MUST BE AN INTEGER
@@ -68,26 +72,28 @@ def load_cache(fpath, random_seed: int=None):
 
     # - footer
     footer = lines[-1]
-    cache_random_seed = int(footer.strip().split()[-1]) # TODO: random state
-    #assert cache_random_seed == random_seed
+    cache_random_seed = int(footer.strip().split()[-1])  # TODO: random state
+    # assert cache_random_seed == random_seed
 
     return raw_markers
+
 
 def group_markers(new_markers_unsorted):
     """"""
     new_markers = sorted(new_markers_unsorted, key=lambda x: x[0])
     raw_markers_unsorted = []
     for k, v in itertools.groupby(new_markers, key=lambda x: x[0]):
-        raw_markers_unsorted.append([k,[x[1] for x in v]])
+        raw_markers_unsorted.append([k, [x[1] for x in v]])
 
     # traj markers are sorted when set
-    raw_markers = [[x[0],sorted(x[1])] for x in sorted(raw_markers_unsorted, key=lambda x:x[0])]
+    raw_markers = [
+        [x[0], sorted(x[1])] for x in sorted(raw_markers_unsorted, key=lambda x: x[0])
+    ]
 
     return raw_markers
 
 
-class AbstractSelector(AbstractNode):
-
+class AbstractSelector(BaseComponent):
     """The base class of any selector."""
 
     #: Selector name.
@@ -97,13 +103,10 @@ class AbstractSelector(AbstractNode):
     axis: Optional[int] = None
 
     #: Default parameters.
-    default_parameters: dict = dict(
-        number = [4, 0.2], # number & ratio
-        verbose = False
-    )
+    default_parameters: dict = dict(number=[4, 0.2], verbose=False)  # number & ratio
 
     #: A worker for potential computations.
-    worker: DriverBasedWorker = None
+    worker: Optional[DriverBasedWorker] = None
 
     #: Distinguish structures when using ComposedSelector.
     prefix: str = "selection"
@@ -127,8 +130,8 @@ class AbstractSelector(AbstractNode):
 
         self.axis = axis
 
-        self.fname = self.name+"-info.txt"
-        
+        self.fname = self.name + "-info.txt"
+
         # - update parameters from kwargs
         self.parameters = copy.deepcopy(self.default_parameters)
         for k in self.parameters:
@@ -152,45 +155,41 @@ class AbstractSelector(AbstractNode):
 
         return object.__getattribute__(self, key)
 
-    @AbstractNode.directory.setter
-    def directory(self, directory_) -> NoReturn:
-        self._directory = pathlib.Path(directory_)
-        self.info_fpath = self._directory/self._fname
+    @BaseComponent.directory.setter
+    def directory(self, directory: Union[str, pathlib.Path]) -> None:
+        self._directory = pathlib.Path(directory).resolve()
+        self.info_fpath = self._directory / self._fname
 
-        return 
-    
+        return
+
     @property
     def fname(self):
         """"""
         return self._fname
-    
+
     @fname.setter
     def fname(self, fname_):
         """"""
         self._fname = fname_
-        self.info_fpath = self._directory/self._fname
+        self.info_fpath = self._directory / self._fname
         return
-    
-    def attach_worker(self, worker=None) -> NoReturn:
+
+    def attach_worker(self, worker=None) -> None:
         """Attach a worker to this node."""
         self.worker = worker
 
         return
 
-    def select(self, inp_dat: AtomsNDArray, *args, **kargs) -> List[Atoms]:
+    def select(self, inp_dat: AtomsNDArray) -> list[Atoms]:
         """Select trajectories.
 
         Based on used selction protocol
 
         Args:
-            frames: A list of ase.Atoms or a list of List[ase.Atoms].
-            index_map: Global indices of frames.
-            ret_indices: Whether return selected indices or frames.
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-        
+            inp_dat: The input AtomsNDArray object.
+
         Returns:
-            List[Atoms] or List[int]: selected results
+            A list of Atoms objects.
 
         """
         self._print(f"@@@{self.__class__.__name__}")
@@ -220,27 +219,27 @@ class AbstractSelector(AbstractNode):
             # -- restart
             self._print("use cached...")
             raw_markers = load_cache(self.info_fpath)
-            #print(f"raw_markers: {raw_markers}")
+            # print(f"raw_markers: {raw_markers}")
             frames.markers = raw_markers
-        
+
         out_nframes = len(frames.markers)
         self._print(f"{self.name} nstructures {inp_nframes} -> nselected {out_nframes}")
 
         # - add history
         #   get_marked_structures return reference to Atoms objects
-        #for a in inp_dat.get_marked_structures():
+        # for a in inp_dat.get_marked_structures():
         #    print(a.info.get("selection"))
 
         marked_structures = inp_dat.get_marked_structures()
         for atoms in marked_structures:
             selection = atoms.info.get("selection", "")
-            atoms.info["selection"] = selection+f"->{self.name}"
-        
-        #for a in inp_dat.get_marked_structures():
+            atoms.info["selection"] = selection + f"->{self.name}"
+
+        # for a in inp_dat.get_marked_structures():
         #    print(a.info["selection"])
 
         return marked_structures
-    
+
     @abc.abstractmethod
     def _mark_structures(self, data, *args, **kwargs) -> None:
         """Mark structures subject to selector's conditions."""
@@ -263,27 +262,27 @@ class AbstractSelector(AbstractNode):
         else:
             assert len(number_info) == 2, "Cant parse number for selection..."
             num_fixed, num_percent = number_info
-        
+
         if num_fixed is not None:
             if num_fixed > nframes:
-                num_fixed = int(nframes*num_percent)
+                num_fixed = int(nframes * num_percent)
         else:
-            num_fixed = int(nframes*num_percent)
+            num_fixed = int(nframes * num_percent)
 
         return num_fixed
 
-    def _write_cached_results(self, aa: AtomsNDArray, *args, **kwargs) -> None:
+    def _write_cached_results(self, aa: AtomsNDArray) -> None:
         """Write selection results into file that can be used for restart."""
-        # - 
+        # -
         markers = aa.markers
 
         # - output
         data = []
         for ind in markers:
-            atoms = aa[tuple(ind)]
+            atoms: Atoms = aa[tuple(ind)]
             # - gather info
             confid = atoms.info.get("confid", -1)
-            step = atoms.info.get("step", -1) # step number in the trajectory
+            step = atoms.info.get("step", -1)  # step number in the trajectory
             natoms = len(atoms)
             try:
                 ene = atoms.get_potential_energy()
@@ -298,18 +297,18 @@ class AbstractSelector(AbstractNode):
             # - add info
             ind_str = ",".join([str(x) for x in ind])
             data.append([f"{ind_str}", confid, step, natoms, ene, ae, maxforce, score])
-        
+
         if data:
             save_cache(self.info_fpath, data, self.random_seed)
         else:
-            #np.savetxt(
+            # np.savetxt(
             #    self.info_fpath, [[np.NaN]*8],
             #    header="{:>11s}  {:>8s}  {:>8s}  {:>8s}  {:>12s}  {:>12s}  {:>12s}  {:>12s}".format(
             #        *"index confid step natoms ene aene maxfrc score".split()
             #    ),
             #    footer=f"random_seed {self.random_seed}"
-            #)
-            content ="{:>11s}  {:>8s}  {:>8s}  {:>8s}  {:>12s}  {:>12s}  {:>12s}  {:>12s}".format(
+            # )
+            content = "{:>11s}  {:>8s}  {:>8s}  {:>8s}  {:>12s}  {:>12s}  {:>12s}  {:>12s}".format(
                 *"index confid step natoms ene aene maxfrc score".split()
             )
             content += f"random_seed {self.random_seed}"
