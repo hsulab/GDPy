600931354e331a537d2cfaeb3372006ff468cb39
hsulab <ahcigar@foxmail.com>
2025-01-06 22:48:26 -0500

    improve: rename AbstractNode to BaseComponent

--- src/gdpx/core/node.py
+++ src/gdpx/core/node.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import abc
-import copy
 import pathlib
-from typing import NoReturn, Optional, Union, Callable
+from typing import Any, Callable, Optional, Union
 
 import numpy as np
+import omegaconf
 
 from . import config
 
 """Every working component is represented by a node.
 """
 
-class AbstractNode(abc.ABC):
+
+class BaseComponent(abc.ABC):
 
     #: Node name.
-    name: str = "node"
-
-    #: The random seed when initialising the object.
-    _init_random_seed: Optional[Union[int, dict]] = None
+    name: str = "component"
 
     #: Standard print function.
     _print: Callable = config._print
@@ -31,28 +30,30 @@ class AbstractNode(abc.ABC):
     default_parameters: dict = dict()
 
     def __init__(
-        self, directory: Union[str,pathlib.Path]="./", 
-        random_seed: Optional[Union[int, dict]]=None, *args, **kwargs
+        self,
+        directory: Union[str, pathlib.Path] = "./",
+        random_seed: Optional[Union[int, dict]] = None,
     ):
         """"""
-        #: Working directory.
+        # Set the working directory.
         self._directory = pathlib.Path(directory)
 
-        # - set random generator
+        # Set the random number generator
         self._init_random_seed = random_seed
+
         self.set_rng(seed=random_seed)
 
-        # - number of processors
+        # Set number of processors
         self.njobs = config.NJOBS
 
         return
-    
+
     @property
-    def init_random_seed(self):
+    def init_random_seed(self) -> Optional[Union[int, dict]]:
         """The random seed at the initialisation of the object.
 
         This cannot be changed after the object is initialised.
-        
+
         """
 
         return self._init_random_seed
@@ -64,33 +65,47 @@ class AbstractNode(abc.ABC):
         Note:
             When setting directory, some additional files are re-directed.
 
+        Returns:
+            The working directory path.
+
         """
+
         return self._directory
-    
+
     @directory.setter
-    def directory(self, directory: Union[str,pathlib.Path]) -> None:
+    def directory(self, directory: Union[str, pathlib.Path]) -> None:
         """"""
         self._directory = pathlib.Path(directory)
 
-        return 
+        return
 
-    def set_rng(self, seed=None):
-        """"""
-        # - save random seed
+    def set_rng(self, seed: Any = None) -> None:
+        """Set the random number generator.
+
+        We must use self.rng to generate any random things for reproducibility.
+
+        """
+        # Get an integer from the global rng if seed is not set,
+        # and store the integer as the seed.
         if seed is None:
-            seed = int(config.GRNG.integers(0, 1e8))
+            seed = int(config.GRNG.integers(0, 1e8))  # type: ignore
+
         self.random_seed = seed
 
-        # - assign random seeds
-        if isinstance(seed, int): # TODO: numpy int?
+        # Set up random seeds
+        if isinstance(seed, int) or isinstance(seed, np.integer):
             self.rng = np.random.Generator(np.random.PCG64(seed))
-        elif isinstance(seed, dict): # TODO: omegaconf dict?
+        elif isinstance(seed, dict) or isinstance(seed, omegaconf.DictConfig):
             self.rng = np.random.Generator(np.random.PCG64())
             self.rng.bit_generator.state = seed
         else:
-            ...
+            raise Exception(f"Unknown random seed `{seed}` type `{type(seed)}`")
 
         return
+
+
+# For backward compatibility
+AbstractNode = BaseComponent
 
 
 if __name__ == "__main__":
