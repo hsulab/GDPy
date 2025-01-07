07ef9d82176868aecf5262beab9fea2b5c2bd6b7
hsulab <ahcigar@foxmail.com>
2025-01-07 00:14:54 -0500

    improve: describer uses BaseComponent

--- src/gdpx/describer/describer.py
+++ src/gdpx/describer/describer.py
@@ -1,33 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import abc
-import copy
-import pathlib
-from typing import Optional, List, Mapping
 
-import numpy as np
+import abc
+
 from sklearn.decomposition import PCA
 
-from ase import Atoms
-from ase.io import read, write
+from gdpx.core.component import BaseComponent
 
-from gdpx.core.node import AbstractNode
-
-try:
-    from dscribe.descriptors import SOAP
-except Exception as e:
-    ...
-
-USE_CHEMISCOPE = 0
 try:
     import chemiscope
-    USE_CHEMISCOPE = 1
 except Exception as e:
-    ...
+    chemiscope = None
 
 
-class AbstractDescriber(AbstractNode):
+class AbstractDescriber(BaseComponent):
 
     cache_features = "features.npy"
 
@@ -45,56 +32,56 @@ class AbstractDescriber(AbstractNode):
 
     def _write_chemiscope(self, frames, features):
         """"""
-        # - write chemiscope inputs
+        assert chemiscope is not None
+
+        # Write chemiscope inputs
         pca = PCA(n_components=2).fit_transform(features)
         properties = dict(
-            PCA = dict(
-                target = "structure",
-                values = pca
-            ),
-            #energies = dict(
+            PCA=dict(target="structure", values=pca),
+            # energies = dict(
             #    target = "structure",
             #    values = [a.get_potential_energy() for a in frames],
             #    units = "eV"
-            #)
+            # )
         )
 
-        frame_properties = chemiscope.extract_properties(
-            frames,
-            only=["energy"]
-        )
+        frame_properties = chemiscope.extract_properties(frames, only=["energy"])
         properties.update(**frame_properties)
 
         chemiscope.write_input(
-            self.directory/"my-input.json.gz", frames=frames, properties=properties
+            self.directory / "my-input.json.gz", frames=frames, properties=properties
         )
 
         return
 
-    def _plot_results(self, features, groups: Mapping[str,List[int]], *args, **kwargs):
+    def _plot_results(self, features, groups: dict[str, list[int]]):
         """"""
         # - plot selection
         import matplotlib.pyplot as plt
+
         try:
             plt.style.use("presentation")
-        except Exception as e:
+        except Exception:
             ...
 
         reducer = PCA(n_components=2)
         reducer.fit(features)
 
         for i, (name, indices) in enumerate(groups.items()):
-            proj = reducer.transform(features[indices,:])
+            proj = reducer.transform(features[indices, :])
             plt.scatter(
-                proj[:, 0], proj[:, 1], alpha=0.5, zorder=100-i,
-                label=f"{name} {len(indices)}"
+                proj[:, 0],
+                proj[:, 1],
+                alpha=0.5,
+                zorder=100 - i,
+                label=f"{name} {len(indices)}",
             )
 
         plt.legend()
         plt.axis("off")
-        plt.savefig(self.directory/"pca.png")
+        plt.savefig(self.directory / "pca.png")
 
-        return 
+        return
 
 
 if __name__ == "__main__":
