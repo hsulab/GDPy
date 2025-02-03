129664c6b1b6efd0122fa7d3c7af925128790b94
jxu <ahcigar@foxmail.com>
2025-02-03 16:30:24 -0500

    refact: use clustering in random

--- src/gdpx/selector/random.py
+++ src/gdpx/selector/random.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-import itertools
-from typing import Optional
-
 import numpy as np
 
-from ..data.array import AtomsNDArray
+from gdpx.data.array import AtomsNDArray
+
+from .clustering import group_structures_by_axis
 from .selector import BaseSelector
 
 
@@ -17,17 +16,15 @@ class RandomSelector(BaseSelector):
 
     default_parameters = dict(number=[4, 0.2])
 
-    """"""
-
     def __init__(self, *args, **kwargs) -> None:
         """"""
         super().__init__(*args, **kwargs)
 
         return
 
-    def _mark_structures(self, data: AtomsNDArray, *args, **kwargs) -> None:
+    def _mark_structures(self, data: AtomsNDArray) -> None:
         """"""
-        marker_groups = self.group_structures_by_axis(data, self.group_by)
+        marker_groups = group_structures_by_axis(data, self.group_by)
 
         selected_markers = []
         for grp_name, markers in marker_groups.items():
@@ -38,33 +35,16 @@ class RandomSelector(BaseSelector):
                     markers, size=num_selected, replace=False
                 )
                 selected_markers.extend(curr_selected_markers)
+                self._print(
+                    f"group: {grp_name} -> "
+                    + f"number of structures: {len(curr_selected_markers)}"
+                )
             else:
                 ...
 
         data.markers = np.array(selected_markers)
 
         return
-
-    @staticmethod
-    def group_structures_by_axis(data: AtomsNDArray, axis: Optional[int] = None):
-        # - group markers
-        if axis is not None:
-            ndim = len(data.shape)
-            if axis < -ndim or axis > ndim:
-                raise IndexError(f"axis {axis} is out of dimension {ndim}.")
-            if axis < 0:
-                axis = ndim + axis
-
-            marker_groups = {}
-            for k, v in itertools.groupby(data.markers, key=lambda x: x[axis]):
-                if k in marker_groups:
-                    marker_groups[k].extend(list(v))
-                else:
-                    marker_groups[k] = list(v)
-        else:
-            marker_groups = dict(all=data.markers)
-
-        return marker_groups
 
 
 if __name__ == "__main__":
