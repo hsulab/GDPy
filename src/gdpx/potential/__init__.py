4b9ba95b740a92c2e095e74cc501c3ea308fe7a3
jxu <ahcigar@foxmail.com>
2025-02-14 15:50:15 -0500

    refact: move potentials out of managers

--- src/gdpx/potential/__init__.py
+++ src/gdpx/potential/__init__.py
@@ -1,17 +1,134 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 
-""" potential wrappers
-    general potential format: deepmd, eann, lasp, vasp
-    dynamics backend: ase, lammps, lasp, vasp
-"""
-
 
 from .. import config
-from ..core.register import registers
+from .. import registers
+from ..manager import AbstractPotentialManager, BasePotentialManager
+from ..trainer import AbstractTrainer
 
-from ..computation.lammps import Lammps
-from ..utils.logio import remove_extra_stream_handlers
+from ..calculators.dummy import DummyCalculator
+from ..calculators.mixer import CommitteeCalculator
+
+from .. import remove_extra_stream_handlers
+
+
+# - basic potentials
+# -- MLIP
+from .deepmd import DeepmdManager, DeepmdTrainer, DeepmdDataloader
+registers.manager.register("deepmd")(DeepmdManager)
+registers.trainer.register(DeepmdTrainer)
+registers.dataloader.register(DeepmdDataloader)
+
+try:
+    from .deepmd import DeepmdJaxManager, DeepmdJaxTrainer
+    registers.manager.register("deepmd-jax")(DeepmdJaxManager)
+    registers.trainer.register(DeepmdJaxTrainer)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`deepmd_jax`':<16s} -> require `{e.name}`.")
+
+from .reann.beann import BeannManager, BeannTrainer
+registers.manager.register("beann")(BeannManager)
+registers.trainer.register(BeannTrainer)
+
+from .reann.reann import ReannManager, ReannTrainer, ReannDataloader
+registers.manager.register("reann")(ReannManager)
+registers.trainer.register(ReannTrainer)
+registers.dataloader.register(ReannDataloader)
+
+from .lasp import LaspManager
+registers.manager.register("lasp")(LaspManager)
+
+from .mace import MaceManager, MaceTrainer, MaceDataloader
+registers.manager.register("mace")(MaceManager)
+registers.trainer.register(MaceTrainer)
+registers.dataloader.register(MaceDataloader)
+
+from .nequip import NequipManager, NequipTrainer
+registers.manager.register("nequip")(NequipManager)
+registers.trainer.register(NequipTrainer)
+
+#try:
+#    from .schnet import SchnetManager
+#    registers.manager.register(SchnetManager)
+#except ImportError as e:
+#    warnings.warn("Module {} import failed: {}".format("schnet", e), UserWarning)
+
+from .mattersim import MatterSimManager
+registers.manager.register("mattersim")(MatterSimManager)
+
+# -- reference potentials
+# --- DFT
+from .cp2k import Cp2kManager
+registers.manager.register("cp2k")(Cp2kManager)
+
+from .espresso import EspressoManager
+registers.manager.register("espresso")(EspressoManager)
+
+from .vasp import VaspManager
+registers.manager.register("vasp")(VaspManager)
+
+# --- FFs
+from .asepot import AsePotManager
+registers.manager.register("ase")(AsePotManager)
+
+from .classic import ClassicManager
+registers.manager.register("classic")(ClassicManager)
+
+from .eam import EamManager
+registers.manager.register("eam")(EamManager)
+
+from .emt import EmtManager
+registers.manager.register("emt")(EmtManager)
+
+from .reax import ReaxManager
+registers.manager.register("reax")(ReaxManager)
+
+# - advanced potentials
+from .grid import GridManager
+registers.manager.register("grid")(GridManager)
+
+from .mixer import MixerManager
+registers.manager.register("mixer")(MixerManager)
+
+# Optional potentials
+try:
+    from .abacus import AbacusManager
+    registers.manager.register("abacus")(AbacusManager)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`abacus`':<16s} -> require `{e.name}`.")
+
+try:
+    from .xtb import XtbManager
+    registers.manager.register("xtb")(XtbManager)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`xtb`':<16s} -> require `{e.name}`.")
+
+try:
+    from .dftd3 import Dftd3Manager
+    registers.manager.register("dftd3")(Dftd3Manager)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`dftd3`':<16s} -> require `{e.name}`.")
+
+try:
+    from .bias import BiasManager
+    registers.manager.register("bias")(BiasManager)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`bias`':<16s} -> require `{e.name}`.")
+
+try:
+    from .plumed.plumed import PlumedManager
+    registers.manager.register("plumed")(PlumedManager)
+except ImportError as e:
+    config._print(f"  {'Potential':<16s} {'`plumed`':<16s} -> require `{e.name}`.")
+
+# - trainers
+from .gp.fgp import FGPTrainer
+registers.trainer.register("FgpTrainer")(FGPTrainer)
+
+from .gp.sgp import SGPTrainer
+registers.trainer.register("SgpTrainer")(SGPTrainer)
+
 
 
 if __name__ == "__main__":
--- src/gdpx/potential/utils.py
+++ src/gdpx/potential/utils.py
@@ -2,62 +2,58 @@
 # -*- coding: utf-8 -*-
 
 
-import copy
 import pathlib
-from typing import Any, Optional
+from typing import Union
 
-import omegaconf
-
-from ..core.register import registers
-from ..core.variable import Variable
-from ..utils.command import parse_input_file
-from .manager import AbstractPotentialManager
+from ..calculators.mixer import CommitteeCalculator
 
 
-def potter_from_dict(inp_dict: dict) -> "AbstractPotentialManager":
-    """"""
-    name = inp_dict.get("name", None)
-    potter = registers.create(
-        "manager",
-        name,
-        convert_name=False,
-    )
-    potter.register_calculator(inp_dict.get("params", {}))
-    potter.version = inp_dict.get("version", "unknown")
+def canonicalise_input_models(model: Union[str, list[str]]) -> list[str]:
+    """Convert input models to a list of resolved path strings.
 
-    return potter
+    Args:
+        model: The input model(s).
 
+    Returns:
+        The resolved path strings. An error is raised if the model does not exist.
 
-def convert_input_to_potter(inp: Any) -> Optional["AbstractPotentialManager"]:
-    """Convert an input to a potter and adjust its behaviour."""
-    potter = None
-    if isinstance(inp, AbstractPotentialManager):
-        potter = inp
-    elif isinstance(inp, Variable):
-        potter = inp.value
-    elif isinstance(inp, dict) or isinstance(
-        inp, omegaconf.dictconfig.DictConfig
-    ):
-        # DictConfig must be cast to dict as sometimes cannot be overwritten.
-        if isinstance(inp, omegaconf.dictconfig.DictConfig):
-            inp = omegaconf.OmegaConf.to_object(inp)
-        assert isinstance(inp, dict)
-        potter_params = copy.deepcopy(inp)
-        potter = potter_from_dict(potter_params)
-    elif isinstance(inp, str) or isinstance(inp, pathlib.Path):
-        if pathlib.Path(inp).exists():
-            potter_params = parse_input_file(input_fpath=inp)
-            potter = potter_from_dict(potter_params)
-        else:
-            raise RuntimeError(
-                f"The potter configuration `{inp}` does not exist."
-            )
+    """
+    if not isinstance(model, list):
+        assert isinstance(model, str)
+        model_ = [model]
     else:
-        raise RuntimeError(
-            f"Unknown {inp} of type {type(inp)} for the potter."
-        )
+        model_ = model
 
-    return potter
+    models = []
+    for m in model_:
+        m = pathlib.Path(m).resolve()
+        if not m.exists():
+            raise FileNotFoundError(f"The model {str(m)} does not exist.")
+        models.append(str(m))
+
+    return models
+
+
+def build_a_committee_calculator(
+    calc_cls, params_list: list[dict], estimate_uncertainty: bool = False
+):
+    """Build a committee calculator.
+
+    If there is one set of parameters or estimate_uncertainty is False, 
+    then a single calculator is built.
+
+    """
+    num_calculators = len(params_list)
+    assert num_calculators >= 1, "At least one calculator must be provided."
+    use_committee = num_calculators > 1 and estimate_uncertainty
+    if use_committee:
+        calc = CommitteeCalculator(
+            calcs=[calc_cls(**params) for params in params_list]
+        )
+    else:
+        calc = calc_cls(**params_list[0])
+
+    return calc
 
 
 if __name__ == "__main__":
