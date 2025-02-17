76d2cb15c928c7474d78203c2f0ad371c437bb94
jxu <ahcigar@foxmail.com>
2025-02-17 11:22:17 -0500

    chores: update typing

--- src/gdpx/data/interface.py
+++ src/gdpx/data/interface.py
@@ -6,26 +6,19 @@ import copy
 import itertools
 import pathlib
 import re
-import warnings
-
-from typing import Any, NoReturn, Optional, List, Mapping
+from typing import Mapping, Optional
 
 import numpy as np
-
 from ase import Atoms
 from ase.io import read, write
 
 from .. import config
+from ..core.operation import Operation
 from ..core.register import registers
 from ..core.variable import Variable
-from ..core.operation import Operation
-
 from .array import AtomsNDArray
-from .dataset import XyzDataloader
 from .system import DataSystem
 
-
-# --- variable
 
 @registers.variable.register
 class TempdataVariable(Variable):
@@ -36,10 +29,10 @@ class TempdataVariable(Variable):
         super().__init__(initial_value)
 
         return
-    
-    def _process_dataset(self, system_dirs: List[str]):
+
+    def _process_dataset(self, system_dirs: list[str]):
         """"""
-        #system_dirs.sort()
+        # system_dirs.sort()
         system_dirs = [pathlib.Path(s) for s in system_dirs]
 
         dataset = []
@@ -62,10 +55,10 @@ class NamedTempdataVariable(Variable):
         super().__init__(initial_value)
 
         return
-    
-    def _process_dataset(self, system_dirs: List[str]):
+
+    def _process_dataset(self, system_dirs: list[str]):
         """"""
-        #system_dirs.sort()
+        # system_dirs.sort()
         system_dirs = [pathlib.Path(s) for s in system_dirs]
 
         data_systems = []
@@ -82,19 +75,20 @@ class DatasetVariable(Variable):
 
     def __init__(self, name, directory="./", *args, **kwargs):
         """"""
-        dataset = registers.create("dataloader", name, convert_name=True, **kwargs)
+        dataset = registers.create(
+            "dataloader", name, convert_name=True, **kwargs
+        )
         super().__init__(initial_value=dataset, directory=directory)
 
         return
 
 
-# --- operation
 @registers.operation.register
 class assemble(Operation):
 
     def __init__(self, variable, directory="./", **kwargs) -> None:
         """"""
-        vkwargs = {} # variable non-variable/operation kwargs
+        vkwargs = {}  # variable non-variable/operation kwargs
         node_names, input_nodes = [], []
         for k, v in kwargs.items():
             if isinstance(v, (Variable, Operation)):
@@ -109,7 +103,7 @@ class assemble(Operation):
         self.node_names = node_names
 
         return
-    
+
     def forward(self, *outputs):
         """"""
         super().forward()
@@ -120,7 +114,7 @@ class assemble(Operation):
             if o is None:
                 is_finished = False
                 break
-        
+
         if is_finished:
             params = copy.deepcopy(self.vkwargs)
             params.update({k: v for k, v in zip(self.node_names, outputs)})
@@ -133,37 +127,37 @@ class assemble(Operation):
 
         return ret
 
+
 @registers.operation.register
 class seqrun(Operation):
 
-    def __init__(self, nodes, *args, **kwargs) -> NoReturn:
+    def __init__(self, nodes, *args, **kwargs) -> None:
         """"""
         super().__init__(nodes)
 
         return
-    
+
     def forward(self, *outputs):
         """"""
         super().forward()
 
         return
 
+
 @registers.operation.register
 class chain(Operation):
+    """Merge arbitrary nodes' outputs into one list."""
 
-    """Merge arbitrary nodes' outputs into one list.
-    """
+    status = "finished"  # Always finished since it is not time-consuming
 
-    status = "finished" # Always finished since it is not time-consuming
-
-    def __init__(self, nodes, *args, **kwargs) -> NoReturn:
+    def __init__(self, nodes, *args, **kwargs) -> None:
         """"""
         super().__init__(nodes)
 
         # - some operation parameters
 
         return
-    
+
     def forward(self, *outputs):
         """"""
         super().forward()
@@ -171,24 +165,26 @@ class chain(Operation):
 
         return list(itertools.chain(*outputs))
 
+
 @registers.operation.register
 class map(Operation):
-
     """Give each input node a name and construct a dict.
 
     This is useful when validating structures from different systems.
 
     """
 
-    def __init__(self, nodes, names, directory="./") -> NoReturn:
+    def __init__(self, nodes, names, directory="./") -> None:
         """"""
         super().__init__(nodes, directory)
 
-        assert len(nodes) == len(names), "Numbers of nodes and names are inconsistent."
+        assert len(nodes) == len(
+            names
+        ), "Numbers of nodes and names are inconsistent."
         self.names = names
 
         return
-    
+
     def forward(self, *outputs):
         """"""
         super().forward()
@@ -196,13 +192,14 @@ class map(Operation):
         ret = {}
         for k, v in zip(self.names, outputs):
             ret[k] = v
-        
+
         self._debug(f"map ret: {ret}")
 
         # NOTE: some dependant nodes may not finish
         self.status = "finished"
 
         return ret
+
 
 @registers.operation.register
 class zip_nodes(Operation):
@@ -222,6 +219,7 @@ class zip_nodes(Operation):
         ret = [list(x) for x in zip(*outputs)]
 
         return ret
+
 
 @registers.operation.register
 class list_nodes(Operation):
@@ -245,15 +243,18 @@ class list_nodes(Operation):
 
 @registers.operation.register
 class transfer(Operation):
-
-    """Transfer worker results to target destination.
-    """
+    """Transfer worker results to target destination."""
 
     def __init__(
-        self, structures, dataset, version, 
-        prefix: str="", system: str="mixed", clean_info: bool=False,
-        set_pbc: bool=True,
-        directory="./"
+        self,
+        structures,
+        dataset,
+        version,
+        prefix: str = "",
+        system: str = "mixed",
+        clean_info: bool = False,
+        set_pbc: bool = True,
+        directory="./",
     ) -> None:
         """"""
         input_nodes = [structures, dataset]
@@ -262,80 +263,79 @@ class transfer(Operation):
         self.version = version
 
         self.prefix = prefix
-        self.system = system # molecule/cluster, surface, bulk
+        self.system = system  # molecule/cluster, surface, bulk
 
-        self.clean_info = clean_info # whether clean atoms info
-        self.set_pbc = set_pbc # Whether set structures to full pbc
+        self.clean_info = clean_info  # whether clean atoms info
+        self.set_pbc = set_pbc  # Whether set structures to full pbc
 
         return
-    
-    def forward(self, frames: List[Atoms], dataset):
+
+    def forward(self, structures: list[Atoms], dataset):
         """"""
         super().forward()
 
-        if isinstance(frames, AtomsNDArray):
-            frames = frames.get_marked_structures()
-        nframes = len(frames)
-        self._print(f"{nframes = }")
+        if isinstance(structures, AtomsNDArray):
+            structures = structures.get_marked_structures()
+        num_structures = len(structures)
+        self._print(f"{num_structures = }")
 
         target_dir = dataset.directory.resolve()
         self._print(f"target dir: {str(target_dir)}")
 
-        # - check chemical symbols
-        system_dict = {} # {formula: [indices]}
+        # Check chemical symbols
+        system_dict = {}  # {formula: [indices]}
 
-        # NOTE: groupby only collects contiguous data
-        #       we need aggregate by ourselves
-        formulae = [a.get_chemical_formula() for a in frames]
+        # We need aggregate by ourselves
+        # as groupby only collects contiguous data
+        formulae = [a.get_chemical_formula() for a in structures]
         for k, v in itertools.groupby(enumerate(formulae), key=lambda x: x[1]):
             if k not in system_dict:
                 system_dict[k] = [x[0] for x in v]
             else:
                 system_dict[k].extend([x[0] for x in v])
-        
-        # - transfer data
-        acc_nframes = 0
+
+        # Transfer data
+        acc_num_structures = 0
         for formula, curr_indices in system_dict.items():
-            # -- TODO: check system type
-            system_type = self.system # currently, use user input one
-            # -- name = description+formula+system_type
-            #dirname = "-".join([self.directory.parent.name, formula, system_type])
+            system_type = self.system  # currently, use user input one
             dirname = "-".join([self.prefix, formula, system_type])
 
-            target_subdir = target_dir/dirname
+            target_subdir = target_dir / dirname
             target_subdir.mkdir(parents=True, exist_ok=True)
 
-            # -- save frames
-            curr_frames = [frames[i] for i in curr_indices]
-            curr_nframes = len(curr_frames)
+            curr_structures = [structures[i] for i in curr_indices]
+            curr_num_frames = len(curr_structures)
 
             if self.set_pbc:
-                for atoms in curr_frames:
+                for atoms in curr_structures:
                     atoms.set_pbc(True)
 
             if self.clean_info:
-                self._clean_frames(curr_frames)
+                self._clean_frames(curr_structures)
 
             strname = self.version + ".xyz"
-            target_destination = target_dir/dirname/strname
+            target_destination = target_dir / dirname / strname
             if not target_destination.exists():
-                write(target_destination, curr_frames)
-                self._print(f"nframes {curr_nframes} -> {str(target_destination.relative_to(target_dir))}")
+                write(target_destination, curr_structures)
+                self._print(
+                    f"num_structures {curr_num_frames} -> {str(target_destination.relative_to(target_dir))}"
+                )
             else:
-                #warnings.warn(f"{target_destination} exists.", UserWarning)
-                self._print(f"WARN: {str(target_destination.relative_to(target_dir))} exists.")
+                self._print(
+                    f"{str(target_destination.relative_to(target_dir))} exists."
+                )
 
-            acc_nframes += curr_nframes
+            acc_num_structures += curr_num_frames
 
-        assert nframes == acc_nframes
-        
+        assert num_structures == acc_num_structures
+
         self.status = "finished"
 
         return dataset
-    
-    def _clean_frames(self, frames: List[Atoms]):
+
+    def _clean_frames(self, structures: list[Atoms]):
         """"""
-        for atoms in frames:
+        for atoms in structures:
             info_keys = copy.deepcopy(list(atoms.info.keys()))
             for k in info_keys:
                 if k not in ["energy", "free_energy"]:
@@ -348,28 +348,34 @@ class transfer(Operation):
 class scope(Operation):
 
     def __init__(
-            self, dataset, describer, 
-            groups: Optional[dict]=None, subgroups: Optional[dict]=None, level: int=0,
-            add_legend: bool=True, write_chemiscope=False, directory="./"
-        ) -> None:
+        self,
+        dataset,
+        describer,
+        groups: Optional[dict] = None,
+        subgroups: Optional[dict] = None,
+        level: int = 0,
+        add_legend: bool = True,
+        write_chemiscope=False,
+        directory="./",
+    ) -> None:
         """"""
         super().__init__(input_nodes=[dataset, describer], directory=directory)
-        
+
         self.level = level
 
         if groups is None:
             groups = {"all": r".*"}
-        self.groups = {k: fr"{v}" for k, v in groups.items()}
+        self.groups = {k: rf"{v}" for k, v in groups.items()}
 
         if subgroups is None:
             subgroups = {"all": r".*"}
-        self.subgroups = {k: fr"{v}" for k, v in subgroups.items()}
+        self.subgroups = {k: rf"{v}" for k, v in subgroups.items()}
 
         self.add_legend = add_legend
         self.write_chemiscope = write_chemiscope
 
         return
-    
+
     def forward(self, dataset, describer):
         """"""
         super().forward()
@@ -382,7 +388,9 @@ class scope(Operation):
         self._debug(f"starts: {starts}")
 
         # - get groups
-        group_indices = {k: {sk: [] for sk in self.subgroups} for k in self.groups}
+        group_indices = {
+            k: {sk: [] for sk in self.subgroups} for k in self.groups
+        }
         for i, system in enumerate(dataset):
             # -- match group
             for k, v in self.groups.items():
@@ -393,13 +401,15 @@ class scope(Operation):
                 continue
             # -- match subgroups
             for sk, sv in self.subgroups.items():
-                curr_indices = [x+starts[i] for x in system.get_matched_indices(sv)]
-                #if sk not in group_indices[k]:
+                curr_indices = [
+                    x + starts[i] for x in system.get_matched_indices(sv)
+                ]
+                # if sk not in group_indices[k]:
                 #    group_indices[k][sk] = curr_indices
-                #else:
+                # else:
                 #    group_indices[k][sk].extend(curr_indices)
                 group_indices[k][sk].extend(curr_indices)
-        #self._debug(f"groups: {group_indices}")
+        # self._debug(f"groups: {group_indices}")
 
         # - merge groups
         merged_groups = {}
@@ -429,10 +439,18 @@ class scope(Operation):
 
         return
 
-    def _plot_results(self, features, groups: Mapping[str,List[int]], add_legend, *args, **kwargs):
+    def _plot_results(
+        self,
+        features,
+        groups: Mapping[str, list[int]],
+        add_legend,
+        *args,
+        **kwargs,
+    ):
         """"""
         # - plot selection
         import matplotlib.pyplot as plt
+
         try:
             plt.style.use("presentation")
         except Exception as e:
@@ -444,65 +462,67 @@ class scope(Operation):
         reducer.fit(features)
 
         # -- separate
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
 
         curves = []
         for i, (name, indices) in enumerate(groups.items()):
             print(f"{name} -> {len(indices)}")
             if len(indices) > 0:
-                proj = reducer.transform(features[indices,:])
+                proj = reducer.transform(features[indices, :])
                 curve = ax.scatter(
-                    proj[:, 0], proj[:, 1], alpha=0.25, zorder=100-i,
-                    label=f"{name} {len(indices)}"
+                    proj[:, 0],
+                    proj[:, 1],
+                    alpha=0.25,
+                    zorder=100 - i,
+                    label=f"{name} {len(indices)}",
                 )
                 curves.append(curve)
         labels = [c.get_label() for c in curves]
 
         ax.axis("off")
-        fig.savefig(self.directory/"pca.png", transparent=True)
+        fig.savefig(self.directory / "pca.png", transparent=True)
 
         label_params = ax.get_legend_handles_labels()
         if add_legend:
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 8))
             ax.axis("off")
             ax.legend(*label_params)
-            fig.savefig(self.directory/"pca-legend.png", transparent=True)
+            fig.savefig(self.directory / "pca-legend.png", transparent=True)
 
-        return 
+        return
 
     def _write_chemiscope(self, frames, features):
         """"""
         USE_CHEMISCOPE = 0
         try:
             import chemiscope
+
             USE_CHEMISCOPE = 1
         except Exception as e:
             print(e)
 
         from sklearn.decomposition import PCA
+
         # - write chemiscope inputs
         pca = PCA(n_components=2).fit_transform(features)
         properties = dict(
-            PCA = dict(
-                target = "structure",
-                values = pca
-            ),
-            #energies = dict(
+            PCA=dict(target="structure", values=pca),
+            # energies = dict(
             #    target = "structure",
             #    values = [a.get_potential_energy() for a in frames],
             #    units = "eV"
-            #)
+            # )
         )
 
         frame_properties = chemiscope.extract_properties(
-            frames,
-            only=["energy"]
+            frames, only=["energy"]
         )
         properties.update(**frame_properties)
 
         chemiscope.write_input(
-            str(self.directory/"chemiscope.json.gz"), 
-            frames=frames, properties=properties
+            str(self.directory / "chemiscope.json.gz"),
+            frames=frames,
+            properties=properties,
         )
 
         return
