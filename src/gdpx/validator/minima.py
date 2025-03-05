4161143926d3c3b46f34b559a3a77e80c8fe8028
jxu <ahcigar@foxmail.com>
2025-03-04 23:41:18 -0500

    feature: add formation energy by references

--- src/gdpx/validator/minima.py
+++ src/gdpx/validator/minima.py
@@ -7,12 +7,14 @@ import pathlib
 from typing import Any, Optional
 
 import numpy as np
+import numpy.typing
 from ase import Atoms
 from ase.calculators.singlepoint import SinglePointCalculator
 from ase.geometry import find_mic
 from ase.io import read, write
 
 from gdpx.builder.builder import StructureBuilder
+from gdpx.factory.builder import canonicalise_builder
 from gdpx.validator.validator import BaseValidator
 from gdpx.worker.drive import DriverBasedWorker
 
@@ -35,7 +37,11 @@ def make_clean_atoms(atoms_, results=None):
     return atoms
 
 
-def compare_structures(v_frames: list[Atoms], p_frames: list[Atoms]):
+def compare_structures(
+    v_frames: list[Atoms],
+    p_frames: list[Atoms],
+    energy_references: Optional[tuple[numpy.typing.NDArray, numpy.typing.NDArray]] = None,
+):
     """"""
     # number of atoms
     v_natoms = np.array([len(a) for a in v_frames])
@@ -45,6 +51,13 @@ def compare_structures(v_frames: list[Atoms], p_frames: list[Atoms]):
     # total energies
     v_ene = np.array([a.get_potential_energy() for a in v_frames])
     p_ene = np.array([a.get_potential_energy() for a in p_frames])
+
+    if energy_references is None:
+        ene_data = (v_ene, p_ene)
+    else:
+        v_f_ene = v_ene - energy_references[0]  # validation formation energy
+        p_f_ene = p_ene - energy_references[1]  # prediction formation energy
+        ene_data = (v_ene, p_ene, v_f_ene, p_f_ene)
 
     # maximum forces TODO: constraints?
     v_maxfrc = np.array([np.max(np.fabs(a.get_forces(apply_constraint=True))) for a in v_frames])
@@ -57,26 +70,55 @@ def compare_structures(v_frames: list[Atoms], p_frames: list[Atoms]):
         _, vlen = find_mic(vector, pre_atoms.get_cell())
         disp.append(np.linalg.norm(vlen))
 
-    results = dict(natoms=v_natoms, ene=(v_ene, p_ene), maxfrc=(v_maxfrc, p_maxfrc), disp=disp)
+    results = dict(natoms=v_natoms, ene=ene_data, maxfrc=(v_maxfrc, p_maxfrc), disp=disp)
 
     return results
 
 
 def summarise_validation(natoms, ene, maxfrc, disp) -> str:
     """"""
-    line_format = "{:>6d}  " * 2 + "{:>12.4f}  " * 7 + "\n"
+    num_ene_columns = len(ene)
 
-    content = (
-        "# Name     N_a  "
-        + ("{:>12s}  " * 7).format("E_v", "E_p", "E_d", "E_d/N_a", "Fmax_v", "Fmax_p", "Disp")
-        + "\n"
-    )
+    if num_ene_columns == 2:
+        line_format = "{:>6d}  " * 2 + "{:>12.4f}  " * 7 + "\n"
 
-    num_structures = len(natoms)
-    for i in range(num_structures):
-        ene_diff = ene[0][i] - ene[1][i]
-        data = [ene[0][i], ene[1][i], ene_diff, ene_diff / natoms[i], maxfrc[0][i], maxfrc[1][i], disp[i]]
-        content += line_format.format(i, natoms[i], *data)
+        content = (
+            "# Name     N_a  "
+            + ("{:>12s}  " * 7).format("E_v", "E_p", "E_d", "E_d/N_a", "Fmax_v", "Fmax_p", "Disp")
+            + "\n"
+        )
+
+        num_structures = len(natoms)
+        for i in range(num_structures):
+            ene_diff = ene[0][i] - ene[1][i]
+            data = [ene[0][i], ene[1][i], ene_diff, ene_diff / natoms[i], maxfrc[0][i], maxfrc[1][i], disp[i]]
+            content += line_format.format(i, natoms[i], *data)
+    elif num_ene_columns == 4:
+        line_format = "{:>6d}  " * 2 + "{:>12.4f}  " * 9 + "\n"
+
+        content = (
+            "# Name     N_a  "
+            + ("{:>12s}  " * 9).format("E_v", "E_p", "E_d", "E_d/N_a", "Ef_v", "Ef_p", "Fmax_v", "Fmax_p", "Disp")
+            + "\n"
+        )
+
+        num_structures = len(natoms)
+        for i in range(num_structures):
+            ene_diff = ene[0][i] - ene[1][i]
+            data = [
+                ene[0][i],
+                ene[1][i],
+                ene_diff,
+                ene_diff / natoms[i],
+                ene[2][i],
+                ene[3][i],
+                maxfrc[0][i],
+                maxfrc[1][i],
+                disp[i],
+            ]
+            content += line_format.format(i, natoms[i], *data)
+    else:
+        raise Exception(f"Unknown number of energy columns: {num_ene_columns}.")
 
     return content
 
@@ -118,6 +160,42 @@ def summarise_validation_with_ranking(natoms, ene, maxfrc, disp) -> str:
     return content
 
 
+def read_reference_structures(inp: list[Any]):
+    """"""
+    energy_list = []
+    for x in inp:
+        if isinstance(x, float):
+            energies = [x]
+        elif isinstance(x, dict):
+            builder = canonicalise_builder(x)
+            assert builder is not None
+            structures = builder.run()
+            energies = [a.get_potential_energy() for a in structures]
+        else:
+            raise Exception(f"Unknown {x} of type {type(x)}.")
+        energy_list.append(energies)
+
+    # Broadcast energies
+    numbers = np.array([len(x) for x in energy_list])
+    num_min = numbers.min()
+    if np.all(numbers == num_min):
+        reference_energies = np.array(energy_list).sum(axis=0)
+    else:
+        if num_min == 1 and np.sum(numbers != num_min) == 1:
+            num_max = numbers.max()
+            new_energy_list = []
+            for energies in energy_list:
+                if len(energies) == 1:
+                    new_energy_list.append(energies * num_max)
+                else:
+                    new_energy_list.append(energies)
+            reference_energies = np.array(new_energy_list).sum(axis=0)
+        else:
+            raise Exception(f"Number of energies are not consistent: {numbers}.")
+
+    return reference_energies
+
+
 class MinimaValidator(BaseValidator):
     """Run minimisation on various configurations and compare relative energy.
 
@@ -129,7 +207,7 @@ class MinimaValidator(BaseValidator):
 
     name: str = "minima"
 
-    def __init__(self, show_ranking: bool=False, *args, **kwargs):
+    def __init__(self, formation_energy: Optional[dict] = None, show_ranking: bool = False, *args, **kwargs):
         """Initialise the validator.
 
         Args:
@@ -139,6 +217,13 @@ class MinimaValidator(BaseValidator):
         super().__init__(*args, **kwargs)
 
         self.show_ranking = show_ranking
+
+        if formation_energy is not None:
+            validation_energies = read_reference_structures(formation_energy["validation"])
+            prediction_energies = read_reference_structures(formation_energy["prediction"])
+            self.reference_energies = (validation_energies, prediction_energies)
+        else:
+            self.reference_energies = None
 
         return
 
@@ -169,7 +254,7 @@ class MinimaValidator(BaseValidator):
 
         end_frames = self._irun(v_structures, v_worker)
         if end_frames is not None:
-            results = compare_structures(v_structures, end_frames)
+            results = compare_structures(v_structures, end_frames, energy_references=self.reference_energies)
             if not pathlib.Path(self.directory / "v.dat").exists():
                 if not self.show_ranking:
                     content = summarise_validation(**results)
