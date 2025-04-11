ff4bee0d3446da7af8b2902c9f36694044c249e2
jxu <ahcigar@foxmail.com>
2025-04-10 21:18:23 -0700

    change: roulette can repeat if all has been used previously

--- src/gdpx/builder/roulette.py
+++ src/gdpx/builder/roulette.py
@@ -2,18 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-import re
 import pathlib
-from typing import List
+import re
 
 import numpy as np
-
 from ase import Atoms
 
 from .builder import StructureModifier
 
-
 active_iteration_pattern = re.compile(r"iter\.[0-9][0-9][0-9][0-9]")
+
 
 def get_current_iteration_from_path(path: pathlib.Path) -> int:
     """"""
@@ -21,7 +19,7 @@ def get_current_iteration_from_path(path: pathlib.Path) -> int:
     num_parts = len(parts)
 
     iteration = -1
-    for i in range(num_parts-1, -1, -1):
+    for i in range(num_parts - 1, -1, -1):
         if re.match(active_iteration_pattern, parts[i]):
             iteration = int(parts[i].split(".")[-1])
             break
@@ -30,13 +28,14 @@ def get_current_iteration_from_path(path: pathlib.Path) -> int:
 
     return iteration
 
+
 def get_previous_path_from_current_by_iteration(path: pathlib.Path) -> pathlib.Path:
     """"""
     parts = path.parts
     num_parts = len(parts)
 
     new_parts = list(parts)
-    for i in range(num_parts-1, -1, -1):
+    for i in range(num_parts - 1, -1, -1):
         if re.match(active_iteration_pattern, parts[i]):
             iteration = int(parts[i].split(".")[-1])
             assert iteration > 0
@@ -48,26 +47,24 @@ def get_previous_path_from_current_by_iteration(path: pathlib.Path) -> pathlib.P
     return pathlib.Path(*new_parts)
 
 
-
 class RouletteBuilder(StructureModifier):
-
-    """Pick one structure randomly from several structures.
-    """
+    """Pick one structure randomly from several structures."""
 
     # TODO: Make this a selector?
 
-    def __init__(self, substrates=None, use_memory: bool=False, *args, **kwargs):
+    def __init__(self, substrates=None, use_memory: bool = False, *args, **kwargs):
         """"""
         super().__init__(substrates=substrates, *args, **kwargs)
 
         self.use_memory = use_memory
 
         return
-    
-    def run(self, substrates=None, size: int=1, *args, **kwargs) -> List[Atoms]:
+
+    def run(self, substrates=None, size: int = 1, *args, **kwargs) -> list[Atoms]:
         """"""
         super().run(substrates=substrates, *args, **kwargs)
 
+        # Check if we have roulette history in active learning
         prev_chosen = []
         if self.use_memory:  # TODO: Check if this builder is in active?
             curr_iter = get_current_iteration_from_path(self.directory)
@@ -75,33 +72,32 @@ class RouletteBuilder(StructureModifier):
             if curr_iter > 0:
                 self._print(">>> Update roulette...")
                 prev_wdir = get_previous_path_from_current_by_iteration(self.directory)
-                prev_chosen = np.loadtxt(prev_wdir/"memory.dat").flatten()
+                prev_chosen = np.loadtxt(prev_wdir / "memory.dat").flatten()
             else:
                 ...
         else:
             ...
+        num_prev_chosen = len(prev_chosen)
 
+        # Choose structures
         num_structures = len(self.substrates)
-
-        test_chosen = self.rng.choice(num_structures, size=np.min([size*10, num_structures]), replace=False)
+        test_chosen = self.rng.choice(num_structures, size=np.min([size * 10, num_structures]), replace=False)
 
         curr_chosen, num_chosen = [], 0
         for i in test_chosen:
             num_chosen = len(curr_chosen)
             if num_chosen >= size:
                 break
-            if i not in prev_chosen:
+            if i not in prev_chosen or num_prev_chosen >= num_structures:
                 curr_chosen.append(i)
-        assert num_chosen == size
+        assert num_chosen == size, f"The chosen indices: {curr_chosen}."
 
-        np.savetxt(self.directory/"memory.dat", np.hstack([prev_chosen, curr_chosen]).T)
+        np.savetxt(self.directory / "memory.dat", np.hstack([prev_chosen, curr_chosen]).T)
 
         self._print(f"{prev_chosen =}  {curr_chosen =}")
 
         return [self.substrates[i] for i in curr_chosen]
 
 
-
 if __name__ == "__main__":
     ...
-  
