5f15796f154e4bca5ee40c4f711844acfb6dbe86
jxu <ahcigar@foxmail.com>
2025-07-30 17:24:33 -0400

    refact(cp2k): band read positions from pos and forces from out

--- src/gdpx/backend/cp2k/parser.py
+++ src/gdpx/backend/cp2k/parser.py
@@ -9,8 +9,6 @@ import numpy as np
 from ase import Atoms, units
 from ase.calculators.singlepoint import SinglePointCalculator
 
-from ase.io import read
-
 
 def read_cp2k_xyz(fpath: pathlib.Path):
     """Read xyz-like file by cp2k.
@@ -295,7 +293,9 @@ def read_cp2k_outputs(wdir: pathlib.Path, prefix: str = "cp2k", task: str = "min
     return frames
 
 
-def read_cp2k_output_from_band(wdir: pathlib.Path, prefix: str="cp2k", print_func=print, debug_func=print) -> list[list[Atoms]]:
+def read_cp2k_output_from_band(
+    wdir: pathlib.Path, prefix: str = "cp2k", print_func=print, debug_func=print
+) -> list[list[Atoms]]:
     """Read the cp2k output from a calculation with `RUN_TYPE BAND`.
 
     CP2K does not save forces to the xyz files, thus, we read forces from the output file.
@@ -401,24 +401,27 @@ def read_cp2k_output_from_band(wdir: pathlib.Path, prefix: str="cp2k", print_fun
                     forces.extend(temp_forces[-1])
                     temp_forces, temp_energies = [], []
 
-    # truncate to the last complete band
+    # Assemble the results into atoms objects
     assert num_atoms is not None, "num_atoms is not read properly."
     assert num_images is not None, "num_images is not read properly."
 
     frames = []  # shape (nbands, nimages)
     if forces:
+        # truncate to the last complete band as output in cp2k.out
         forces = np.array(forces, dtype=np.float64)
         shape = forces.shape
         debug_func(f"forces: {shape}")
-        num_bands = int(shape[0] / num_images)
-        forces = forces[: num_bands * num_images]
-        debug_func(f"truncated forces: {forces.shape} nbands: {num_bands}")
-        forces = np.reshape(forces, (num_bands, num_images, num_atoms, -1))  # shape (nbands, nimages, natoms, 3)
+        num_bands_in_out = int(shape[0] / num_images)
+        forces = forces[: num_bands_in_out * num_images]
+        debug_func(f"truncated forces: {forces.shape} nbands: {num_bands_in_out}")
+        forces = np.reshape(
+            forces, (num_bands_in_out, num_images, num_atoms, -1)
+        )  # shape (nbands, nimages, natoms, 3)
         forces *= units.Hartree / units.Bohr
 
-        energies = np.array(energies)[: num_bands * num_images].reshape(num_bands, num_images)
+        energies = np.array(energies)[: num_bands_in_out * num_images].reshape(num_bands_in_out, num_images)
         energies *= units.Hartree
-        debug_func(f"energies: {energies.shape} nbands: {num_bands}")
+        debug_func(f"energies: {energies.shape} nbands: {num_bands_in_out}")
 
         cell = np.array(cell, dtype=np.float64)
         debug_func(f"cell: {cell}")
@@ -430,8 +433,34 @@ def read_cp2k_output_from_band(wdir: pathlib.Path, prefix: str="cp2k", print_fun
                 curr_xyzfile = wdir / f"cp2k-pos-Replica_nr_{i+1}-1.xyz"
             else:
                 curr_xyzfile = wdir / f"cp2k-pos-Replica_nr_{str(i+1).zfill(2)}-1.xyz"
-            curr_frames = read(curr_xyzfile, index=":", format="xyz")[:num_bands]
+            # curr_frames = read(curr_xyzfile, index=":", format="xyz")[:num_bands]
+            frame_steps, frame_symbols, frame_energies, frame_positions = read_cp2k_xyz(curr_xyzfile)
+            curr_frames = []
+            for _, symbols, positions, energy in zip(frame_steps, frame_symbols, frame_positions, frame_energies):
+                if num_atoms != len(symbols):
+                    raise Exception(
+                        f"Number of atoms {num_atoms} does not match the number of symbols {len(symbols)} in {curr_xyzfile}."
+                    )
+                atoms = Atoms(symbols, positions=np.array(positions, dtype=np.float64), cell=cell, pbc=True)
+                spc = SinglePointCalculator(atoms, energy=energy * units.Hartree, free_energy=energy * units.Hartree)
+                atoms.calc = spc
+                curr_frames.append(atoms)
             band_frames.append(curr_frames)
+
+        # check if all replicas have the same number of frames
+        num_bands_in_replicas = [len(band_frames[i]) for i in range(num_images)]
+        num_bands_in_pos = min(num_bands_in_replicas)
+        print(f"{num_bands_in_out=}  {num_bands_in_pos=}")
+        num_bands = num_bands_in_pos
+        if all([nb == num_bands_in_pos for nb in num_bands_in_replicas]):
+            ...
+        else:
+            print_func(
+                f"Number of bands in replicas is not the same: {num_bands_in_replicas}. "
+                f"Truncating to the minimum number of bands {num_bands}."
+            )
+            for i in range(num_images):
+                band_frames[i] = band_frames[i][:num_bands]
 
         # reshape frames_ to (num_bands, num_images)
         for j in range(num_bands):
@@ -446,13 +475,20 @@ def read_cp2k_output_from_band(wdir: pathlib.Path, prefix: str="cp2k", print_fun
                 atoms = frames[i][j]
                 atoms.set_cell(cell)
                 atoms.pbc = True
-                spc = SinglePointCalculator(
-                    atoms,
-                    energy=energies[i, j],
-                    free_energy=energies[i, j],
-                    forces=forces[i, j].copy(),
-                )
-                atoms.calc = spc
+                if i < num_bands_in_out:
+                    ene_in_pos = atoms.get_potential_energy()
+                    assert np.isclose(
+                        ene_in_pos, energies[i, j]
+                    ), f"Energy in positions {ene_in_pos} does not match the output energy {energies[i, j]} for band {i} image {j}."
+                    spc = SinglePointCalculator(
+                        atoms,
+                        energy=energies[i, j],
+                        free_energy=energies[i, j],
+                        forces=forces[i, j].copy(),
+                    )
+                    atoms.calc = spc
+                else:
+                    ...  # no forces available in the output.
     else:
         ...  # no forces available indicating that not even on band step is finished.
 
