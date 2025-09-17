cc560625a6bae8fefc454ff36d427c345b3c8dc5
jxu <ahcigar@foxmail.com>
2025-09-17 11:54:33 -0400

    feat(geo): support custom molecules

--- src/gdpx/geometry/composition.py
+++ src/gdpx/geometry/composition.py
@@ -8,11 +8,11 @@ from typing import List, Union
 import ase
 import numpy as np
 from ase import Atoms
-from ase.io import read
 from ase.build import molecule
 from ase.collections import g2
 from ase.data import atomic_numbers
 from ase.formula import Formula
+from ase.io import read
 
 from gdpx.utils.strconv import string_to_integers
 
@@ -24,6 +24,16 @@ def convert_string_to_atoms(species: str) -> Atoms:
         atoms = Atoms(species, positions=[[0.0, 0.0, 0.0]])
     elif species in g2.names:
         atoms = molecule(species)
+    elif species == "CHOO":
+        atoms = Atoms(
+            "CHOO",
+            positions=[
+                [8.95, 9.83, 11.71],
+                [8.95, 10.03, 12.80],
+                [10.08, 9.75, 11.15],
+                [7.80, 9.73, 11.16],
+            ],
+        )
     elif species.endswith(".xyz"):
         frames = read(species, ":")  # TODO: check non-pbc molecule only?
         assert len(frames) == 1, f"Only one frame is expected in `{species}`."
@@ -46,15 +56,11 @@ def get_chemical_species_from_kwpairs(name: str, number: Union[int, str]):
     if isinstance(number, int):
         species = [(name, number)]
     elif isinstance(number, str):
-        numbers = string_to_integers(
-            number, convention="lmp", out_convention="lmp"
-        )
+        numbers = string_to_integers(number, convention="lmp", out_convention="lmp")
         for num in numbers:
             species.append((name, num))
     else:
-        raise RuntimeError(
-            f"number must be int or str but `{number}` is given."
-        )
+        raise RuntimeError(f"number must be int or str but `{number}` is given.")
 
     return species
 
@@ -67,9 +73,7 @@ class CompositionSpace:
         if isinstance(composition, dict):
             entries = []
             for k, v in composition.items():
-                entries.append(
-                    get_chemical_species_from_kwpairs(name=k, number=v)
-                )
+                entries.append(get_chemical_species_from_kwpairs(name=k, number=v))
             # Sort species by name to make the composition order consistent
             # Though we will sort fragments in insert
             entries = sorted(entries, key=lambda e: e[0])
@@ -81,9 +85,7 @@ class CompositionSpace:
 
         # Something like [(('H', 2), ('O', 1))]
         self._compositions = _compositions
-        assert (
-            len(self._compositions) > 0
-        ), f"`{_compositions}` must have at least one choice."
+        assert len(self._compositions) > 0, f"`{_compositions}` must have at least one choice."
 
         return
 
@@ -106,9 +108,7 @@ class CompositionSpace:
 
         return chemical_numbers
 
-    def get_fragments_from_one_composition(
-        self, rng=np.random.default_rng()
-    ) -> List[Atoms]:
+    def get_fragments_from_one_composition(self, rng=np.random.default_rng()) -> List[Atoms]:
         """"""
         num_compositions = len(self._compositions)
         idx = rng.choice(num_compositions, size=1, replace=False)[0]
@@ -116,10 +116,7 @@ class CompositionSpace:
 
         fragments = list(
             itertools.chain(
-                *[
-                    [convert_string_to_atoms(name) for _ in range(number)]
-                    for (name, number) in composition
-                ]
+                *[[convert_string_to_atoms(name) for _ in range(number)] for (name, number) in composition]
             )
         )
 
