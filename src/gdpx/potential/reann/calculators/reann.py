977e800a57441304aaab1327b298b14222813a9e
jxu <ahcigar@foxmail.com>
2025-11-12 02:59:09 -0500

    refact(potential): reann calc

--- src/gdpx/potential/reann/calculators/reann.py
+++ src/gdpx/potential/reann/calculators/reann.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*
 
 
+from typing import Optional
+
 import numpy as np
 import torch
+from ase import Atoms
 from ase.calculators.calculator import Calculator, all_changes
 from ase.neighborlist import neighbor_list
 
@@ -27,7 +30,7 @@ class REANN(Calculator):
         dtype: str = "float32",
         **kwargs,
     ):
-        Calculator.__init__(self, **kwargs)
+        super().__init__(**kwargs)
 
         self.device = torch.device(device)
 
@@ -45,8 +48,6 @@ class REANN(Calculator):
         self.pes = None
         self.cutoff = None
 
-        self.table = 0
-
         return
 
     def _init_model(self) -> None:
@@ -59,27 +60,31 @@ class REANN(Calculator):
 
         return
 
-    def calculate(self, atoms=None, properties=["energy", "force"], system_changes=all_changes):
-        Calculator.calculate(self, atoms, properties, system_changes)
+    def calculate(self, atoms: Optional[Atoms] = None, properties=["energy", "force"], system_changes=all_changes):
+        super().calculate(atoms, properties, system_changes)
         if self.pes is None:
             self._init_model()
 
-        positions = torch.from_numpy(atoms.positions).contiguous().to(self.device).to(self.dtype)
+        if atoms is None:
+            raise Exception("Atoms object should not be None when calling calculate.")
 
         i, j, S = neighbor_list("ijS", atoms, cutoff=self.cutoff)
         pairs = torch.from_numpy(np.vstack([i, j])).contiguous().to(self.device).to(torch.long)
         shifts = torch.from_numpy(np.dot(S, atoms.cell)).contiguous().to(self.device).to(self.dtype)
 
+        positions = torch.from_numpy(atoms.positions).contiguous().to(self.device).to(self.dtype)
+
         symbols = list(self.atoms.symbols)
         species = [self.atomtype.index(i) for i in symbols]
         species = torch.tensor(species, device=self.device, dtype=torch.long)
+
         energy, force = self.pes(positions, pairs, shifts, species)
 
         energy = float(energy.detach().cpu().numpy())
         force = force.detach().cpu().numpy()
 
         self.results["energy"] = energy
-        self.results["forces"] = force.copy()
+        self.results["forces"] = force
 
         return
 
