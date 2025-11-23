fd686947863247c4663978a367fc2c0a7363e192
jxu <ahcigar@foxmail.com>
2025-11-23 06:45:14 +0000

    fix(reann): pot with stress

--- src/gdpx/potential/reann/calculators/reann.py
+++ src/gdpx/potential/reann/calculators/reann.py
@@ -19,8 +19,7 @@ model here, which avoids serialisation problem when copying this calculator.
 
 
 class REANN(Calculator):
-
-    implemented_properties = ["energy", "forces"]
+    implemented_properties = ["energy", "forces", "stress"]
 
     def __init__(
         self,
@@ -47,6 +46,7 @@ class REANN(Calculator):
         self._nn_path = nn
         self.pes = None
         self.cutoff = None
+        self.with_stress = False
 
         return
 
@@ -57,6 +57,11 @@ class REANN(Calculator):
         pes.eval()
         self.cutoff = pes.cutoff
         self.pes = torch.jit.optimize_for_inference(pes)
+
+        # We need check whether the script supports stress
+        arg_names = [arg.name for arg in self.pes.forward.schema.arguments]
+        if "disp_cell" in arg_names:
+            self.with_stress = True
 
         return
 
@@ -78,13 +83,48 @@ class REANN(Calculator):
         species = [self.atomtype.index(i) for i in symbols]
         species = torch.tensor(species, device=self.device, dtype=torch.long)
 
-        energy, force = self.pes(positions, pairs, shifts, species)
+        if self.with_stress:
+            cell = np.array(atoms.get_cell(complete=True))
+            tcell = torch.from_numpy(cell).to(self.dtype).to(self.device)
 
-        energy = float(energy.detach().cpu().numpy())
-        force = force.detach().cpu().numpy()
+            disp_cell = torch.zeros_like(tcell)
+            if "forces" in properties:
+                positions.requires_grad = True
+            else:
+                positions.requires_grad = False
 
-        self.results["energy"] = energy
-        self.results["forces"] = force
+            if "stress" in properties:
+                disp_cell.requires_grad = True
+            else:
+                disp_cell.requires_grad = False
+
+            energy = self.pes(tcell, disp_cell, positions, pairs, shifts, species)
+            self.results["energy"] = float(energy.detach().cpu().numpy())
+
+            if "forces" in properties and "stress" in properties:
+                forces, virial = torch.autograd.grad(energy, [positions, disp_cell])
+                forces = torch.neg(forces).detach().cpu().numpy()
+                self.results["forces"] = forces
+                virial = virial.detach().cpu().numpy()
+                self.results["stress"] = virial / atoms.get_volume()
+            elif "forces" in properties and "stress" not in properties:
+                forces = torch.autograd.grad(energy, positions)[0]
+                forces = torch.neg(forces).detach().cpu().numpy()
+                self.results["forces"] = forces
+            elif "stress" in properties and "forces" not in properties:
+                virial = torch.autograd.grad(energy, disp_cell)[0]
+                virial = virial.detach().cpu().numpy()
+                self.results["stress"] = virial / atoms.get_volume()
+            else:
+                raise Exception(f"Unknown {properties=}.")
+        else:
+            energy, force = self.pes(positions, pairs, shifts, species)
+
+            energy = float(energy.detach().cpu().numpy())
+            force = force.detach().cpu().numpy()
+
+            self.results["energy"] = energy
+            self.results["forces"] = force
 
         return
 
