5d4f857897ee88ea83173c89e0a5441cb3e71691
jxu <ahcigar@foxmail.com>
2025-12-18 16:44:14 -0500

    feat(msd): support com

--- src/gdpx/validator/mean_squared_displacement.py
+++ src/gdpx/validator/mean_squared_displacement.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import Optional, Union
+from typing import Callable, Optional, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -83,7 +83,7 @@ def plot_msd(
         else:
             raise Exception(f"Invalid shape {y.shape}.")
 
-    ax.set_ylabel("MSD [Å^2]")
+    ax.set_ylabel(r"MSD [Å$^2$]")
     ax.set_xlabel("Time [ps]")
 
     ax.legend(fontsize=12)
@@ -94,17 +94,22 @@ def plot_msd(
 
 
 def compute_mean_squared_displacement(
-    frames: list[Atoms], group_indices: list[int], lagmax: int, start: int, end: int, timeintv: float
+    frames: list[Atoms],
+    lagmax: int,
+    start: int,
+    end: int,
+    timeintv: float,
+    get_group_positions: Callable,
 ):
     """Compute mean squared displacement (MSD) for a group of atoms.
 
     Args:
         frames: List of ASE Atoms objects representing the trajectory frames.
-        group_indices: Indices of atoms in the group for which to compute the MSD.
         lagmax: Maximum lag time for MSD calculation.
         start: Start index for the trajectory frames.
         end: End index for the trajectory frames.
         timeintv: Time interval between frames in femtoseconds.
+        get_group_positions: Function to extract group positions from Atoms object.
 
     Returns:
         lagtimes: Array of lag times in picoseconds.
@@ -117,10 +122,10 @@ def compute_mean_squared_displacement(
 
     positions = []
     for atoms in frames:
-        positions.append(atoms.get_positions()[group_indices, :])
+        positions.append(get_group_positions(atoms))
     positions = np.array(positions)
 
-    nframes, natoms, _ = positions.shape
+    _, natoms, _ = positions.shape
 
     # Compute the mean squared displacement (MSD) for each lag time.
     msds_by_particle = np.zeros((lagmax, natoms))
@@ -140,21 +145,21 @@ def compute_mean_squared_displacement(
 
 def compute_mean_squared_displacement_over_trajectories(
     frames_list: list[list[Atoms]],
-    group_indices: list[int],
     lagmax: int,
     start: Optional[int],
     end: Optional[int],
     timeintv: float,
+    get_group_positions: Callable,
 ):
     """Compute mean squared displacement (MSD) for a group of atoms.
 
     Args:
         frames_list: List of lists of ASE Atoms objects representing the trajectory frames.
-        group_indices: Indices of atoms in the group for which to compute the MSD.
         lagmax: Maximum lag time for MSD calculation.
         start: Start index for the trajectory frames.
         end: End index for the trajectory frames.
         timeintv: Time interval between frames in femtoseconds.
+        get_group_positions: Function to extract group positions from Atoms object.
 
     Returns:
         lagtimes: Array of lag times in picoseconds.
@@ -169,12 +174,12 @@ def compute_mean_squared_displacement_over_trajectories(
 
         positions = []
         for atoms in frames:
-            positions.append(atoms.get_positions()[group_indices, :])
-        positions = np.array(positions)  # (nframes, natoms, 3)
+            positions.append(get_group_positions(atoms))
+        positions = np.array(positions)  # (num_frames, num_atoms, 3)
 
         positions_list.append(positions)
 
-    nframes, natoms, _ = positions_list[0].shape
+    _, _, _ = positions_list[0].shape
 
     # Compute the mean squared displacement (MSD) for each lag time.
     msd_avg = np.zeros((lagmax, 1))
@@ -185,7 +190,7 @@ def compute_mean_squared_displacement_over_trajectories(
         disp2_list = []
         for positions in positions_list:
             disp = positions[:-lag, :, :] - positions[lag:, :, :]
-            sqdist = np.square(disp).sum(axis=-1)  # (nframes-lag, natoms)
+            sqdist = np.square(disp).sum(axis=-1)  # (nframes-lag, num_atoms)
             disp2 = np.mean(sqdist, axis=1)  # (nframes-lag,)
             disp2_list.append(disp2)
         disp2 = np.concatenate(disp2_list, axis=0)  # (nframes-lag,)
@@ -211,7 +216,6 @@ class MeanSquaredDisplacementValidator(BaseValidator):
 
     def __init__(
         self,
-        group,
         timeintv: float,
         lagmax: int,
         start: Optional[int] = None,
@@ -219,6 +223,8 @@ class MeanSquaredDisplacementValidator(BaseValidator):
         d_start: int = -1,
         d_end: int = 20,
         merge_trajs: bool = False,
+        group: Optional[str] = None,
+        com_group: Optional[str] = None,
         directory: Union[str, pathlib.Path] = "./",
         *args,
         **kwargs,
@@ -227,6 +233,7 @@ class MeanSquaredDisplacementValidator(BaseValidator):
         super().__init__(directory, *args, **kwargs)
 
         self.group = group
+        self.com_group = com_group
 
         self.start = start
         self.end = end
@@ -286,23 +293,51 @@ class MeanSquaredDisplacementValidator(BaseValidator):
 
         """
         mdtrajs = self._process_data(data)
-        group_indices = evaluate_group_expression(mdtrajs[0][0], self.group)
+
+        # TODO: Check trajectory consistency in number of atoms, elements, pbc...
         chemical_symbols = mdtrajs[0][0].get_chemical_symbols()
-        group_formula = Formula.from_list([chemical_symbols[i] for i in group_indices]).convert("metal")
-        self._print(f"num_atoms in the group: {len(group_indices)} formula: {group_formula}")
-        self._debug(f"group_indices: {group_indices}")
+        if self.group is not None:
+            group_indices = evaluate_group_expression(mdtrajs[0][0], self.group)
+            group_formula = Formula.from_list([chemical_symbols[i] for i in group_indices]).convert("metal")
+            self._print(f"num_atoms in the group: {len(group_indices)} formula: {group_formula}")
+            self._debug(f"group_indices: {group_indices}")
+        else:
+            group_indices = None
+
+        if self.com_group is not None:
+            com_group_indices = evaluate_group_expression(mdtrajs[0][0], self.com_group)
+            com_group_formula = Formula.from_list([chemical_symbols[i] for i in com_group_indices]).convert("metal")
+            self._print(f"num_atoms in the com_group: {len(com_group_indices)} formula: {com_group_formula}")
+            self._debug(f"com_group_indices: {com_group_indices}")
+            # Check index in com_group should be a subset of group_indices
+            if group_indices is not None:
+                if not set(com_group_indices).issubset(set(group_indices)):
+                    raise RuntimeError("com_group should be a subset of group.")
+        else:
+            com_group_indices = None
 
         cache_msd = self.directory / f"{prefix}msd.npy"
         if not cache_msd.exists():
+            # Check if we use center of mass
+            if com_group_indices is not None:
+                get_group_positions = lambda atoms: atoms.get_positions()[com_group_indices, :].mean(axis=0)[
+                    np.newaxis, :
+                ]
+            else:
+                if group_indices is not None:
+                    get_group_positions = lambda atoms: atoms.get_positions()[group_indices, :]
+                else:
+                    get_group_positions = lambda atoms: atoms.get_positions()
+
             if not self.merge_trajs:
                 raw_data = Parallel(n_jobs=self.njobs)(
                     delayed(compute_mean_squared_displacement)(
                         [a for a in frames if a is not None],  # AtomsNDArray may have None...
-                        group_indices,
                         lagmax=self.lagmax,
                         start=self.start,
                         end=self.end,
                         timeintv=self.timeintv,
+                        get_group_positions=get_group_positions,
                     )
                     for frames in mdtrajs
                 )
@@ -310,11 +345,11 @@ class MeanSquaredDisplacementValidator(BaseValidator):
             else:
                 raw_data = compute_mean_squared_displacement_over_trajectories(
                     [[a for a in frames if a is not None] for frames in mdtrajs],
-                    group_indices,
                     lagmax=self.lagmax,
                     start=self.start,
                     end=self.end,
                     timeintv=self.timeintv,
+                    get_group_positions=get_group_positions,
                 )
                 data = np.vstack(raw_data)[np.newaxis, :]  # (1, 3, lagmax)
             np.save(cache_msd, data)
