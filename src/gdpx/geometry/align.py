87ededdfa0b192511046a40e10c3ea87dd9f76a3
jxu <ahcigar@foxmail.com>
2025-12-19 17:31:49 -0500

    refact(geo): clean up

--- src/gdpx/geometry/align.py
+++ src/gdpx/geometry/align.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-import copy
-
 from ase import Atoms
 from ase.geometry import find_mic
 
@@ -11,28 +5,23 @@ from ase.geometry import find_mic
 def wrap_traj(frames: list[Atoms]):
     """Align positions according to the first frame.
 
-    This is necessary for computing physical quantities base on atomic positions 
+    This is necessary for computing physical quantities base on atomic positions
     with periodic boundary conditions.
 
     NOTE:
         This only works for fixed cell systems.
-    
+
     TODO:
         Variable cell systems?
 
     """
     cell = frames[0].get_cell(complete=True)
     nframes = len(frames)
-    for i in range(1,nframes):
-        prev_positions = frames[i-1].get_positions()
+    for i in range(1, nframes):
+        prev_positions = frames[i - 1].get_positions()
         curr_positions = frames[i].get_positions()
         shift = curr_positions - prev_positions
         curr_vectors, _ = find_mic(shift, cell, pbc=True)
         frames[i].positions = prev_positions + curr_vectors
 
     return frames
-
-
-if __name__ == "__main__":
-    ...
-  
