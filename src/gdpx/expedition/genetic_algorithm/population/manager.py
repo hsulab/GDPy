7c7a302fdfdd10305a824977baebc57a8bed95c1
jxu <ahcigar@foxmail.com>
2025-12-24 17:01:43 -0500

    refact(ga): use database function

--- src/gdpx/expedition/genetic_algorithm/population/manager.py
+++ src/gdpx/expedition/genetic_algorithm/population/manager.py
@@ -434,24 +434,12 @@ class PopulationManager:
                 frames = generator.run(size=1)
                 if frames:
                     atoms = frames[0]
-                    atoms.info["key_value_pairs"] = {
-                        "extinct": 0,
-                        "origin": "RandomCandidateUnrelaxed",
-                    }
-                    atoms.info["data"] = {}
-                    confid = database.connection.write(
+                    database.add_unrelaxed_candidate(
                         atoms,
-                        relaxed=0,
-                        extinct=0,
-                        random=1,
-                        description="random",
+                        description="random: OffspringGenerator",
+                        origin="RandomCandidateUnrelaxed",
                         generation=curr_gen,
-                        key_value_pairs=atoms.info["key_value_pairs"],
-                        data=atoms.info["data"],
                     )
-                    database.connection.update(confid, confid=confid)
-                    atoms.info["confid"] = confid
-
                     random_structures.append(atoms)
                     self._print(
                         f"  confid={atoms.info['confid']:>6d} parents={'none':<14s} origin={atoms.info['key_value_pairs']['origin']:<20s} extinct={atoms.info['key_value_pairs']['extinct']:<4d}"
@@ -486,24 +474,13 @@ class PopulationManager:
             assert isinstance(parent, Atoms)
             atoms, desc = operators["mobile"]["mutations"].get_new_individual([parent])
             if atoms is not None:
-                _, desc = desc.split(":")
-                atoms.info["key_value_pairs"]["generation"] = curr_gen
-                atoms.info["data"] = {"parents": [parent.info["confid"]]}
-                confid = database.connection.write(
+                database.add_unrelaxed_candidate(
                     atoms,
-                    relaxed=0,
-                    extinct=0,
-                    mutation=1,
                     description=desc,
+                    origin="MutationCandidateUnrelaxed",
                     generation=curr_gen,
-                    key_value_pairs=atoms.info["key_value_pairs"],
-                    data=atoms.info["data"],
                 )
-                database.connection.update(confid, confid=confid)
-                atoms.info["confid"] = confid
-
                 mutated_structures.append(atoms)
-
                 parents = " ".join([str(x) for x in atoms.info["data"]["parents"]])
                 self._print(
                     f"  confid={atoms.info['confid']:>6d} parents={parents:<14s} origin={atoms.info['key_value_pairs']['origin']:<20s} extinct={atoms.info['key_value_pairs']['extinct']:<4d}"
