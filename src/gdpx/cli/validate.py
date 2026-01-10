90f4f12a28d2e3c5d03456ba325017cc30061fdd
jxu <ahcigar@foxmail.com>
2026-01-09 20:29:43 -0500

    feat(val): cli validate workers

--- src/gdpx/cli/validate.py
+++ src/gdpx/cli/validate.py
@@ -1,10 +1,11 @@
 import pathlib
 from typing import Union
 
+from gdpx.factory.computer import canonicalise_worker
 from gdpx.factory.validator import canonicalise_validator
 
 
-def run_validation(config: dict, directory: Union[str, pathlib.Path], potter):
+def run_validation(config: dict, directory: Union[str, pathlib.Path], worker):
     """This is a factory to deal with various validations..."""
     # run over validations
     directory = pathlib.Path(directory)
@@ -12,6 +13,23 @@ def run_validation(config: dict, directory: Union[str, pathlib.Path], potter):
     tasks = config.get("tasks", [])
     if not tasks:
         raise Exception("No tasks found in the configuration.")
+
+    # Assign worker to each task, priority is task-specific > task-global > command
+    task_global_worker_params = config.get("worker", {})
+    if task_global_worker_params:
+        # override the worker from command line
+        task_global_worker = canonicalise_worker(task_global_worker_params)
+    else:
+        task_global_worker = worker
+
+    for task in tasks:
+        task_worker = task.get("worker", {})
+        if task_worker:
+            task["worker"] = task_worker
+        elif task_global_worker:
+            task["worker"] = task_global_worker
+        elif worker:
+            task["worker"] = worker
 
     # Instantiate teh validators
     validators = []
